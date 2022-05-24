ca75037017bae0c4b3a960fc2c5db4fc08d59708
Olav Møyner <olav.moyner@sintef.no>
2022-05-24 11:32:55 +0200

    Update reservoir model to support separate context

--- src/utils.jl
+++ src/utils.jl
@@ -6,7 +6,7 @@ reservoir_storage(model, storage) = storage
 reservoir_storage(model::MultiModel, storage) = storage.Reservoir
 
 export setup_reservoir_model
-function setup_reservoir_model(reservoir, system; wells = [], block_backend = true, context = nothing, reference_densities = nothing)
+function setup_reservoir_model(reservoir, system; wells = [], block_backend = true, context = DefaultContext(), reservoir_context = nothing, reference_densities = nothing)
     # List of models (order matters)
     models = OrderedDict{Symbol, Jutul.AbstractSimulationModel}()
     # Support either a pre-discretized domain, a mesh or geometry
@@ -14,19 +14,17 @@ function setup_reservoir_model(reservoir, system; wells = [], block_backend = tr
     main_domain(m::Jutul.AbstractJutulMesh) = main_domain(tpfv_geometry(m))
     main_domain(geo::Jutul.JutulGeometry) = discretized_domain_tpfv_flow(geo)
 
-    if isnothing(context)
-        context = DefaultContext()
+    if isnothing(reservoir_context)
         if block_backend
-            main_context = DefaultContext(matrix_layout = BlockMajorLayout())
+            @assert context isa DefaultContext
+            reservoir_context = DefaultContext(matrix_layout = BlockMajorLayout())
         else
-            main_context = context
+            reservoir_context = context
         end
-    else
-        main_context = context
     end
     # We first set up the reservoir
     D = main_domain(reservoir)
-    models[:Reservoir] = SimulationModel(D, system, context = main_context)
+    models[:Reservoir] = SimulationModel(D, system, context = reservoir_context)
     # Then we set up all the wells
     for w in wells
         D_w = discretized_domain_well(w)
