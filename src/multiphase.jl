b18d3c897f7acb4ffbf1c0bb45fd4601ff0c4f32
Olav Møyner <olav.moyner@gmail.com>
2022-06-02 22:46:30 +0200

    Avoid AD in convergence criterion

--- src/multiphase.jl
+++ src/multiphase.jl
@@ -256,9 +256,9 @@ function convergence_criterion(model::SimulationModel{D, S}, storage, eq::Conser
     Φ = v(storage.state.FluidVolume)
     ρ = v(storage.state.PhaseMassDensities)
 
-    @timeit "cnv" @tullio max e[j] := abs(r[j, i]) * dt / (value(ρ[j, i])*Φ[i])
+    @timeit "cnv" @tullio max e[j] := abs(r[j, i]) * dt / (value(ρ[j, i])*value(Φ[i]))
     @timeit "mb" begin
-        scale = dt./(sum(Φ).*mean(value, ρ, dims = 2))
+        scale = dt./(sum(value, Φ).*mean(value, ρ, dims = 2))
         mb = scale.*sum(abs, r, dims = 2)
     end
 
