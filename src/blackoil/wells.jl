cf67455f46a2abde73e0f68af59b9a92cb65797d
Olav Møyner <olav.moyner@gmail.com>
2022-06-30 15:05:24 +0200

    Add basic well impl. for blackoil with new crossterm

--- src/blackoil/wells.jl
+++ src/blackoil/wells.jl
@@ -1,3 +1,58 @@
+function well_perforation_flux!(out, sys::BlackOilSystem, state_res, state_well, rhoS, WI, rc, wc)
+    p_res = state_res.Pressure
+    p_well = state_well.Pressure
+
+
+    μ = state_res.PhaseViscosities
+    kr = state_res.RelativePermeabilities
+    ρ = state_res.PhaseMassDensities
+    rs = state_res.Rs
+    b = state_res.ShrinkageFactors
+    # s = state_res.Saturations
+
+    ρ_w = state_well.PhaseMassDensities
+    s_w = state_well.Saturations
+    rs_w = state_well.Rs
+    b_w = state_well.ShrinkageFactors
+    # TODO: Fix the pressure drop
+    ρgdz = 0
+    a, l, v = 1, 2, 3
+    dp = WI*(p_well[wc] - p_res[rc] + ρgdz)
+    λ_a = kr[a, rc]/μ[a, rc]
+    λ_l = kr[l, rc]/μ[l, rc]
+    λ_v = kr[v, rc]/μ[v, rc]
+    rhoOS = rhoS[l]
+    rhoGS = rhoS[v]
+
+    if dp >= 0
+        # Injection
+        λ_t = λ_a + λ_l + λ_v
+        Q = λ_t*dp
+
+        bO = b_w[l, wc]
+        bG = b_w[v, wc]
+        rs_i = rs_w[wc]
+
+        sO = s_w[l, wc]
+        sG = s_w[v, wc]
+
+        q_a = s_w[a, wc]*ρ_w[a, wc]*Q
+        q_l = sO*rhoOS*bO*Q
+        q_v = rhoGS*(sG*bG + rs_i*sO*bO)*Q
+    else
+        # Production
+        bO = b[l, rc]
+        bG = b[v, rc]
+
+        q_a = dp*ρ[a, rc]*λ_a
+        q_l = dp*bO*λ_l*rhoOS
+        q_v = dp*(bO*λ_l*rs[rc] + bG*λ_v)*rhoGS
+    end
+    out[a] = q_a
+    out[l] = q_l
+    out[v] = q_v
+end
+
 function apply_well_reservoir_sources!(sys::BlackOilSystem, res_q, well_q, state_res, state_well, param_res, param_well, perforations, sgn)
     p_res = state_res.Pressure
     p_well = state_well.Pressure
--- src/facility/cross_terms.jl
+++ src/facility/cross_terms.jl
@@ -3,3 +3,22 @@ struct ReservoirWellCrossTerm{T<:AbstractVector, I<:AbstractVector} <: Jutul.Add
     reservoir_cells::I
     well_cells::I
 end
+
+Jutul.symmetry(::ReservoirWellCrossTerm) = Jutul.CTSkewSymmetry()
+
+function update_cross_term_in_entity!(out, i, state_t, state0_t,
+    state_s, state0_s, 
+    model_t, model_s,
+    param_t, param_s,
+    ct::ReservoirWellCrossTerm, eq, dt, ldisc = local_discretization(ct, i))
+    # Unpack properties
+    reservoir_cell = ct.reservoir_cells[i]
+    well_cell = ct.well_cells[i]
+    WI = ct.WI[i]
+    rhoS = param_s[:reference_densities]
+    # Call smaller interface that is easy to specialize
+    well_perforation_flux!(out, model_t.system, state_t, state_s, rhoS, WI, reservoir_cell, well_cell)
+end
+
+Jutul.cross_term_entities(ct::ReservoirWellCrossTerm, eq::ConservationLaw, model) = ct.reservoir_cells
+Jutul.cross_term_entities_source(ct::ReservoirWellCrossTerm, eq::ConservationLaw, model) = ct.well_cells
