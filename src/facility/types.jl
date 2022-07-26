59cf7d5516c4e8da7a055fd7935ee390963e3662
Olav Møyner <olav.moyner@gmail.com>
2022-07-26 13:50:38 +0200

    Fix bug in well sign

--- src/facility/types.jl
+++ src/facility/types.jl
@@ -265,7 +265,7 @@ function (D::WellSegmentFlow)(i, ::Cells)
     cd = D.cell_discretizations
     loc = cd.face_pos[i]:(cd.face_pos[i+1]-1)
     faces = @views cd.faces[loc]
-    signs = @views cd.faces[loc]
+    signs = @views cd.face_sign[loc]
     cells = @views cd.cells[loc]
     return (faces = cd.faces[loc], signs = signs, cells = cells)
 end
