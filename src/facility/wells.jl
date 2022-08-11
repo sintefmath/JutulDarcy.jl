604984d0ec17bb7ca395bf81c921e6379121d72c
Olav Møyner <olav.moyner@gmail.com>
2022-08-11 22:06:19 +0200

    Make well concretely typed

--- src/facility/wells.jl
+++ src/facility/wells.jl
@@ -51,27 +51,25 @@ struct SimpleWell <: WellGrid
     volumes
     perforations
     surface
-    reservoir_symbol
     name::Symbol     # Symbol that names the well
     function SimpleWell(reservoir_cells; name = :Well, reference_depth = 0, volume = 1e-3, reservoir_symbol = :Reservoir, surface_conditions = default_surface_cond(), kwarg...)
         nr = length(reservoir_cells)
 
         WI, gdz = common_well_setup(nr; kwarg...)
         perf = (self = ones(Int64, nr), reservoir = vec(reservoir_cells), WI = WI, gdz = gdz)
-        new([volume], perf, surface_conditions, reservoir_symbol, name)
+        new([volume], perf, surface_conditions, name)
     end
 end
 
-struct MultiSegmentWell <: WellGrid
-    volumes          # One per cell
-    perforations     # (self -> local cells, reservoir -> reservoir cells, WI -> connection factor)
-    neighborship     # Well cell connectivity
-    top              # "Top" node where scalar well quantities live
-    centers          # Coordinate centers of nodes
-    surface          # p, T at surface
-    reservoir_symbol # Symbol of the reservoir the well is coupled to
-    name::Symbol     # Symbol that names the well
-    segment_models   # Segment pressure drop model
+struct MultiSegmentWell{V, P, N, A, C, SC, S} <: WellGrid
+    volumes::V          # One per cell
+    perforations::P     # (self -> local cells, reservoir -> reservoir cells, WI -> connection factor)
+    neighborship::N     # Well cell connectivity
+    top::A              # "Top" node where scalar well quantities live
+    centers::C          # Coordinate centers of nodes
+    surface::SC          # p, T at surface
+    name::Symbol             # Symbol that names the well
+    segment_models::S        # Segment pressure drop model
     function MultiSegmentWell(reservoir_cells, volumes::AbstractVector, centers;
                                                         N = nothing,
                                                         name = :Well,
@@ -118,7 +116,7 @@ struct MultiSegmentWell <: WellGrid
         WI, gdz = common_well_setup(nr; dz = dz, kwarg...)
         perf = (self = perforation_cells, reservoir = reservoir_cells, WI = WI, gdz = gdz)
         accumulator = (reference_depth = reference_depth, )
-        new(volumes, perf, N, accumulator, ext_centers, surface_conditions, reservoir_symbol, name, segment_models)
+        new{typeof(volumes), typeof(perf), typeof(N), typeof(accumulator), typeof(ext_centers), typeof(surface_conditions), typeof(segment_models)}(volumes, perf, N, accumulator, ext_centers, surface_conditions, name, segment_models)
     end
 end
 
