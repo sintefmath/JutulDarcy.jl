e2ea125eca692231c76c6be868dd18df7ac24b90
Olav Møyner <olav.moyner@gmail.com>
2022-08-25 15:24:44 +0200

    Update blackoil flux (not well tested)

--- src/blackoil/flux.jl
+++ src/blackoil/flux.jl
@@ -1,3 +1,42 @@
+@inline function component_mass_fluxes!(q, face, state, model::SimulationModel{<:Any, <:BlackOilSystem, <:Any, <:Any}, kgrad, upw)
+    sys = model.system
+    a, l, v = phase_indices(sys)
+    rhoS = reference_densities(sys)
+    # Aqueous phase is straightforward, just the mass flux for phase
+    q_a = darcy_phase_mass_flux(face, a, state, model, kgrad, upw)
+    # Get the potentials since the flux is more complicated for miscible phases
+    ψ_l = darcy_phase_kgrad_potential(face, l, state, model, kgrad)
+    ψ_v = darcy_phase_kgrad_potential(face, v, state, model, kgrad)
+
+    kr = state.RelativePermeabilities
+    μ = state.PhaseViscosities
+    b = state.ShrinkageFactors
+
+    # Oil
+    f_bl = (cell, flag) -> b_mob(b, kr, μ, l, cell, flag)
+    λb_l = upwind(upw, f_bl, ψ_l)
+    q_l = rhoS[l]*λb_l*ψ_l
+    # Gas
+    f_bv = (cell, flag) -> b_mob(b, kr, μ, v, cell, flag)
+    λb_v = upwind(upw, f_bv, ψ_v)
+
+    f_rs = (cell, flag) -> @inbounds b[v, cell]
+    # Note: Rs is upwinded by liquid potential
+    rs = upwind(upw, f_rs, ψ_l)
+    q_v = (λb_v*ψ_v + rs*λb_l*ψ_l)*rhoS[v]
+
+    q = setindex(q, q_a, a)
+    q = setindex(q, q_l, l)
+    q = setindex(q, q_v, v)
+    return q
+end
+
+function b_mob(b, kr, μ, ph, c, flag)
+    @inbounds λ = kr[ph, c]/μ[ph, c]
+    @inbounds b_f = b[ph, c]
+    return λ*b_f
+end
+
 """
 Half face Darcy flux (Blackoil version)
 """
