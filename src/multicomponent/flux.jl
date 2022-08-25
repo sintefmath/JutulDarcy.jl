32199937e48b6790f2a79dc3c74ed60756cad021
Olav Møyner <olav.moyner@gmail.com>
2022-08-25 16:11:33 +0200

    Re-Implement compositional flux

--- src/multicomponent/flux.jl
+++ src/multicomponent/flux.jl
@@ -1,3 +1,45 @@
+@inline function component_mass_fluxes!(q, face, state, model::SimulationModel{<:Any, <:CompositionalSystem, <:Any, <:Any}, kgrad, upw)
+    # for ph in eachindex(q)
+    #     q_i = darcy_phase_mass_flux(face, ph, state, model, kgrad, upw)
+    #     @inbounds q = setindex(q, q_i, ph)
+    # end
+    sys = model.system
+    aqua = Val(has_other_phase(sys))
+    ph_ix = phase_indices(sys)
+
+    X = state.LiquidMassFractions
+    Y = state.VaporMassFractions
+    kr = state.RelativePermeabilities
+    μ = state.PhaseViscosities
+    ρ = state.PhaseMassDensities
+    pressure = state.Pressure
+    s = state.Saturations
+
+    q = compositional_fluxes!(q, face, state,  X, Y, kr, μ, ρ, pressure, s, model, kgrad, upw, aqua, ph_ix)
+    return q
+end
+
+@inline function compositional_fluxes!(q, face, state, X, Y, kr, μ, ρ, pressure, s, model, kgrad, upw, aqua::Val{false}, phase_ix)
+    nc = size(X, 1)
+    l, v = phase_ix
+    q_l = darcy_phase_mass_flux(face, l, state, model, kgrad, upw)
+    q_v = darcy_phase_mass_flux(face, v, state, model, kgrad, upw)
+
+    q = inner_compositional!(q, X, Y, q_l, q_v, upw, nc)
+    return q
+end
+
+@inline function inner_compositional!(q, X, Y, q_l, q_v, upw, nc, offset = 0)
+    for i in 1:nc
+        X_f = upwind(upw, cell -> @inbounds(X[i, cell]), q_l)
+        Y_f = upwind(upw, cell -> @inbounds(Y[i, cell]), q_v)
+
+        q_i = q_l*X_f + q_v*Y_f
+        q = setindex(q, q_i, i + offset)
+    end
+    return q
+end
+
 """
 Half face Darcy flux (Compositional version)
 """
