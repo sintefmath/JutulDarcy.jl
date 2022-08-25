e806cb5d5d5c1608b3bcdcbd1a14e6429861d5cd
Olav Møyner <olav.moyner@gmail.com>
2022-08-25 13:32:45 +0200

    Clean up and inline

--- src/flux.jl
+++ src/flux.jl
@@ -4,28 +4,36 @@ function single_unique_potential(model)
     return model.domain.discretizations.mass_flow.gravity
 end
 
-function Jutul.compute_tpfa_flux!(q_i, left, right, face, face_sign, eq, state, model, dt, flow_disc)
+@inline function Jutul.compute_tpfa_flux!(q_i, left, right, face, face_sign, eq, state, model, dt, flow_disc)
     return component_mass_fluxes!(q_i, face, state, model, TPFA(left, right, face_sign), SPU(left, right))
 end
 
-@inline function pressure_gradient(state, tpfa::TPFA)
-    P = state.Pressure
-    return @inbounds P[tpfa.left] - P[tpfa.right]
+@inline function component_mass_fluxes!(q, face, state, model::SimulationModel{<:Any, <:Union{ImmiscibleSystem, SinglePhaseSystem}, <:Any, <:Any}, kgrad, upw)
+    for ph in eachindex(q)
+        q_i = darcy_phase_mass_flux(face, ph, state, model, kgrad, upw)
+        @inbounds q = @set q[ph] = q_i
+    end
+    return q
 end
 
-function darcy_phase_kgrad_potential(face, phase, state, model, tpfa::TPFA)
+@inline function darcy_phase_mass_flux(face, phase, state, model, kgrad, upw)
+    Q = darcy_phase_kgrad_potential(face, phase, state, model, kgrad)
+    ρλ = c -> immiscible_phase_mass_mobility(state, phase, c)
+    ρλ_f = upwind(upw, ρλ, Q)
+    return ρλ_f*Q
+end
 
+@inline function darcy_phase_kgrad_potential(face, phase, state, model, tpfa::TPFA)
     trans = state.Transmissibilities
     grav = state.TwoPointGravityDifference
-
     ρ = state.PhaseMassDensities
     pc, ref_index = capillary_pressure(model, state)
     ∇p = pressure_gradient(state, tpfa)
-
-    return darcy_phase_flux_inner(∇p, pc, ref_index, trans, face, phase, grav, ρ, tpfa)
+    q = darcy_phase_flux_inner(∇p, pc, ref_index, trans, face, phase, grav, ρ, tpfa)
+    return q
 end
 
-function darcy_phase_flux_inner(∇p, pc, ref_index, trans, face, phase, grav, ρ, tpfa)
+@inline function darcy_phase_flux_inner(∇p, pc, ref_index, trans, face, phase, grav, ρ, tpfa)
     l = tpfa.left
     r = tpfa.right
     s = tpfa.face_sign
@@ -37,19 +45,17 @@ function darcy_phase_flux_inner(∇p, pc, ref_index, trans, face, phase, grav, �
 
     @inbounds ρ_c = ρ[phase, l]
     @inbounds ρ_i = ρ[phase, r]
-
     ρ_avg = 0.5*(ρ_i + ρ_c)
-    return -T_f*(∇p + Δpc + gΔz*ρ_avg)
+    q = -T_f*(∇p + Δpc + gΔz*ρ_avg)
+    return q
 end
 
-function darcy_phase_mass_flux(face, phase, state, model, kgrad, upw)
-    Q = darcy_phase_kgrad_potential(face, phase, state, model, kgrad)
-    ρλ = c -> immiscible_phase_mass_mobility(state, phase, c)
-    ρλ_f = upwind(upw, ρλ, Q)
-    return ρλ_f*Q
+@inline function pressure_gradient(state, tpfa::TPFA)
+    P = state.Pressure
+    return @inbounds P[tpfa.left] - P[tpfa.right]
 end
 
-function upwind(upw::SPU, F, q)
+@inline function upwind(upw::SPU, F, q)
     if q >= 0
         up = upw.right
     else
@@ -58,130 +64,12 @@ function upwind(upw::SPU, F, q)
     return F(up)
 end
 
-function immiscible_phase_mass_mobility(state, ph, c)
-    ρ = state.PhaseMassDensities
-    kr = state.RelativePermeabilities
-    μ = state.PhaseViscosities
-    return @inbounds ρ[ph, c]*kr[ph, c]/μ[ph, c]
+@inline function immiscible_phase_mass_mobility(state, ph, c)
+    @inbounds kr = state.RelativePermeabilities[ph, c]
+    @inbounds ρ = state.PhaseMassDensities[ph, c]
+    @inbounds μ = state.PhaseViscosities[ph, c]
+    return ρ*kr/μ
 end
-
-function component_mass_fluxes!(q, face, state, model::SimulationModel{<:Any, <:Union{ImmiscibleSystem, SinglePhaseSystem}, <:Any, <:Any}, kgrad, upw)
-    for ph in eachindex(q)
-        q_i = darcy_phase_mass_flux(face, ph, state, model, kgrad, upw)
-        q = @set q[ph] = q_i
-    end
-    return q
-end
-
-function update_half_face_flux!(flux::AbstractArray, state, model, param, dt, flow_disc::TwoPointPotentialFlowHardCoded)
-    rho, kr, mu, p = state.PhaseMassDensities, state.RelativePermeabilities, state.PhaseViscosities, state.Pressure
-    pc, ref_index = capillary_pressure(model, state)
-    conn_data = flow_disc.conn_data
-    ctx = model.context
-    single_potential = !flow_disc.gravity && isnothing(pc)
-    phases = get_phases(model.system)
-    update_immiscible_fluxes!(flux, ctx, conn_data, rho, kr, mu, p, pc, ref_index, phases, single_potential)
-end
-
-function update_immiscible_fluxes!(flux, context, conn_data, rho, kr, mu, p, pc, ref_index, phases, single_potential)
-    mb = minbatch(context)
-    if single_potential
-        @batch minbatch = mb for i in eachindex(conn_data)
-            immiscible_multiphase_flux_single_pot!(flux, i, conn_data, phases, p, rho, kr, mu)
-        end
-    else
-        # Multiphase potential
-        @batch minbatch = mb for i in eachindex(conn_data)
-            immiscible_multiphase_flux_multi_pot!(flux, i, conn_data, phases, p, rho, kr, mu, pc, ref_index)
-        end
-    end
-end
-
-function update_immiscible_fluxes!(flux, context::SingleCUDAContext, conn_data, rho, kr, mu, p, pc, ref_index, single_potential)
-    if single_potential
-        # Scalar potential
-        @tullio flux[ph, i] = immiscible_flux_for_phase_single_pot(conn_data[i], ph, p, rho, kr, mu)
-    else
-        # Multiphase potential
-        @tullio flux[ph, i] = immiscible_flux_for_phase_multi_pot(conn_data[i], ph, p, rho, kr, mu, pc, ref_index)
-    end
-end
-
-function immiscible_multiphase_flux_multi_pot!(flux, conn_i, conn_data, phases, p, rho, kr, mu, pc, ref_index)
-    @inbounds cd = conn_data[conn_i]
-    self, other, gΔz, T = cd.self, cd.other, cd.gdz, cd.T
-    ∂ = (x) -> local_ad(x, self)
-
-    kr = ∂(kr)
-    mu = ∂(mu)
-    rho = ∂(rho)
-    p = ∂(p)
-    pc = ∂(pc)
-    for ph in eachindex(phases)
-        @inbounds flux[ph, conn_i] = immiscible_flux_gravity(self, other, ph, kr, mu, rho, p, pc, T, gΔz, ref_index)
-    end
-end
-
-@inline function immiscible_flux_for_phase_multi_pot(cd, ph, p, rho, kr, mu, pc, ref_index)
-    c, i, gΔz, T = cd.self, cd.other, cd.gdz, cd.T
-    ∂ = (x) -> local_ad(x, c)
-    return immiscible_flux_gravity(c, i, ph, ∂(kr), ∂(mu), ∂(rho), ∂(p), ∂(pc), T, gΔz, ref_index)
-end
-
-@inline function immiscible_flux_gravity(c, i, ph, kᵣ, μ, ρ, P, pc, T, gΔz, ref_index)
-    @inbounds ρ_c = ρ[ph, c]
-    @inbounds ρ_i = ρ[ph, i]
-    ρ_avg = (ρ_i + ρ_c)/2
-    Δpc = capillary_gradient(pc, c, i, ph, ref_index)
-    @inbounds Δp = P[c] - P[i] + Δpc
-    θ = -T*(Δp + gΔz*ρ_avg)
-    if θ < 0
-        # Flux is leaving the cell
-        up = c
-        ρ = ρ_c
-    else
-        # Flux is entering the cell
-        up = i
-        ρ = ρ_i
-    end
-    @inbounds ρλᶠ = ρ*kᵣ[ph, up]/μ[ph, up]
-    return ρλᶠ*θ
-end
-
-
-function immiscible_multiphase_flux_single_pot!(flux, conn_i, conn_data, phases, p, rho, kr, mu)
-    @inbounds cd = conn_data[conn_i]
-    self, other, T = cd.self, cd.other, cd.T
-    ∂ = (x) -> local_ad(x, self)
-
-    kr = ∂(kr)
-    mu = ∂(mu)
-    rho = ∂(rho)
-    p = ∂(p)
-    for ph in eachindex(phases)
-        @inbounds flux[ph, conn_i] = immiscible_flux_no_gravity(self, other, ph, kr, mu, rho, p, T)
-    end
-end
-
-function immiscible_flux_for_phase_single_pot(cd, ph, p, rho, kr, mu)
-    c, i, T = cd.self, cd.other, cd.T
-    ∂ = (x) -> local_ad(x, c)
-    return immiscible_flux_no_gravity(c, i, ph, ∂(kr), ∂(mu), ∂(rho), ∂(p), T)
-end
-
-function immiscible_flux_no_gravity(c, i, ph, kᵣ, μ, ρ, P, T)
-    @inbounds θ = -T*(P[c] - P[i])
-    if θ < 0
-        # Flux is leaving the cell
-        up_c = c
-    else
-        # Flux is entering the cell
-        up_c = i
-    end
-    @inbounds ρλᶠ = ρ[ph, up_c]*kᵣ[ph, up_c]/μ[ph, up_c]
-    return ρλᶠ*θ
-end
-
 
 capillary_gradient(::Nothing, c_l, c_r, ph, ph_ref) = 0.0
 function capillary_gradient(pc, c_l, c_r, ph, ph_ref)
