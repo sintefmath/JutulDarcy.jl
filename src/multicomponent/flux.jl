76b97b0e99211059c1633377631980ba5581d213
Olav Møyner <olav.moyner@gmail.com>
2022-08-25 16:16:48 +0200

    Add water compositional flux

--- src/multicomponent/flux.jl
+++ src/multicomponent/flux.jl
@@ -29,13 +29,25 @@ end
     return q
 end
 
-@inline function inner_compositional!(q, X, Y, q_l, q_v, upw, nc, offset = 0)
+@inline function compositional_fluxes!(q, face, state, X, Y, kr, μ, ρ, pressure, s, model, kgrad, upw, aqua::Val{true}, phase_ix)
+    nc = size(X, 1)
+    a, l, v = phase_ix
+    q_a = darcy_phase_mass_flux(face, a, state, model, kgrad, upw)
+    q_l = darcy_phase_mass_flux(face, l, state, model, kgrad, upw)
+    q_v = darcy_phase_mass_flux(face, v, state, model, kgrad, upw)
+
+    q = inner_compositional!(q, X, Y, q_l, q_v, upw, nc)
+    q = setindex(q, q_a, nc+1)
+    return q
+end
+
+@inline function inner_compositional!(q, X, Y, q_l, q_v, upw, nc)
     for i in 1:nc
         X_f = upwind(upw, cell -> @inbounds(X[i, cell]), q_l)
         Y_f = upwind(upw, cell -> @inbounds(Y[i, cell]), q_v)
 
         q_i = q_l*X_f + q_v*Y_f
-        q = setindex(q, q_i, i + offset)
+        q = setindex(q, q_i, i)
     end
     return q
 end
