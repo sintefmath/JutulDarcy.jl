227406a9e6c5ae49710a8ac9df3e384faa0bcec8
Olav Møyner <olav.moyner@gmail.com>
2022-09-13 13:05:53 +0200

    Update objectives.jl

--- src/gradients/objectives.jl
+++ src/gradients/objectives.jl
@@ -1,6 +1,6 @@
 export well_mismatch
 
-function compute_qoi(well_model, state, well::Symbol, pos, rhoS, control)
+function compute_well_qoi(well_model, state, well::Symbol, pos, rhoS, control)
     well_state = state[well]
     well_state = convert_to_immutable_storage(well_state)
 
@@ -15,35 +15,36 @@ function compute_qoi(well_model, state, well::Symbol, pos, rhoS, control)
     return v
 end
 
-function well_mismatch(model_f, model_c, states_f, states_c, tstep, forces, well::Symbol, qoi)
-    well_f = model_f[well]
-    well_c = model_c[well]
-    rhoS = reference_densities(well_f.system)
-    pos = get_well_position(model_f.models[:Facility].domain, well)
+# function well_mismatch(model_f, model_c, states_f, states_c, tstep, forces, well::Symbol, qoi)
+#     well_f = model_f[well]
+#     well_c = model_c[well]
+#     rhoS = reference_densities(well_f.system)
+#     pos = get_well_position(model_f.models[:Facility].domain, well)
 
-    t_tot = sum(tstep)
-    mismatch = 0.0
-    for i in eachindex(tstep)
-        dt = tstep[i]
-        force = Jutul.forces_for_timestep(nothing, forces, tstep, i)
-        ctrl = force[:Facility].control[well]
-        ctrl = replace_target(ctrl, qoi)
+#     t_tot = sum(tstep)
+#     mismatch = 0.0
+#     for i in eachindex(tstep)
+#         dt = tstep[i]
+#         force = Jutul.forces_for_timestep(nothing, forces, tstep, i)
+#         ctrl = force[:Facility].control[well]
+#         ctrl = replace_target(ctrl, qoi)
 
-        state_f = states_f[i]
-        state_c = states_c[i]
+#         state_f = states_f[i]
+#         state_c = states_c[i]
 
-        qoi_f = compute_qoi(well_f, state_f, well, pos, rhoS, ctrl)
-        qoi_c = compute_qoi(well_c, state_c, well, pos, rhoS, ctrl)
+#         qoi_f = compute_qoi(well_f, state_f, well, pos, rhoS, ctrl)
+#         qoi_c = compute_qoi(well_c, state_c, well, pos, rhoS, ctrl)
 
-        mismatch += (dt/t_tot)*(qoi_f - qoi_c)^2
-    end
-    return sqrt(mismatch)
-end
+#         mismatch += (dt/t_tot)*(qoi_f - qoi_c)^2
+#     end
+#     return sqrt(mismatch)
+# end
 
-function well_mismatch(qoi, well, model_f, states_f, model_c, state_c, dt, step_no, forces; weights = ones(length(qoi)))
+function well_mismatch(qoi, well, model_f, states_f, model_c, state_c, dt, step_no, forces; weights = ones(length(qoi)), scale = 1.0)
     if !(qoi isa AbstractArray)
         qoi = [qoi]
     end
+    @assert length(weights) == length(qoi)
     pos = get_well_position(model_c.models[:Facility].domain, well)
 
     well_f = model_f[well]
@@ -57,11 +58,10 @@ function well_mismatch(qoi, well, model_f, states_f, model_c, state_c, dt, step_
     obj = 0.0
     for (i, q) in enumerate(qoi)
         ctrl = replace_target(ctrl, q)
-        qoi_f = compute_qoi(well_f, state_f, well, pos, rhoS, ctrl)
-        qoi_c = compute_qoi(well_c, state_c, well, pos, rhoS, ctrl)
+        qoi_f = compute_well_qoi(well_f, state_f, well, pos, rhoS, ctrl)
+        qoi_c = compute_well_qoi(well_c, state_c, well, pos, rhoS, ctrl)
 
         obj += weights[i]*(qoi_f - qoi_c)^2
     end
-
-    return dt*obj
+    return scale*dt*obj
 end
