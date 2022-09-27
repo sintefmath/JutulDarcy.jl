40a0fac1453dd3fc4548bdd700330e12635ffe60
Olav Møyner <olav.moyner@gmail.com>
2022-09-27 18:32:23 +0200

    Region support for capillary pressure

--- src/multiphase_secondary_variables.jl
+++ src/multiphase_secondary_variables.jl
@@ -202,8 +202,18 @@ end
     end
 end
 
-struct SimpleCapillaryPressure{T} <: GroupedVariables
+struct SimpleCapillaryPressure{T, R} <: GroupedVariables
     pc::T
+    regions::R
+    function SimpleCapillaryPressure(pc::C; regions::T = nothing) where {C, T}
+        is_tup_tup = first(pc) isa Tuple
+        if isnothing(regions)
+            @assert !is_tup_tup
+        end
+        pc = map(x -> region_wrap(x, regions), pc)
+        pc = tuple(pc...)
+        return new{typeof(pc), T}(pc)
+    end
 end
 
 degrees_of_freedom_per_entity(model, v::SimpleCapillaryPressure) = number_of_phases(model.system) - 1
@@ -214,6 +224,8 @@ degrees_of_freedom_per_entity(model, v::SimpleCapillaryPressure) = number_of_pha
     if npc == 1
         pcow = cap[1]
         @inbounds for c in 1:nc
+            reg = region(pc.regions, c)
+            pcow_c = table_by_region(pcow, reg)
             sw = Saturations[1, c]
             Δp[1, c] = pcow(sw)
         end
@@ -221,22 +233,29 @@ degrees_of_freedom_per_entity(model, v::SimpleCapillaryPressure) = number_of_pha
         pcow, pcog = cap
         if isnothing(pcow)
             @inbounds for c in 1:nc
+                reg = region(pc.regions, c)
+                pcog_c = table_by_region(pcog, reg)
                 sg = Saturations[3, c]
                 Δp[1, c] = 0
-                Δp[2, c] = pcog(sg)
+                Δp[2, c] = pcog_c(sg)
             end
         elseif isnothing(pcog)
             @inbounds for c in 1:nc
+                reg = region(pc.regions, c)
+                pcow_c = table_by_region(pcow, reg)
                 sw = Saturations[1, c]
-                Δp[1, c] = -pcow(sw)
+                Δp[1, c] = -pcow_c(sw)
                 Δp[2, c] = 0
             end
         else
             @inbounds for c in 1:nc
+                reg = region(pc.regions, c)
+                pcow_c = table_by_region(pcow, reg)
+                pcog_c = table_by_region(pcog, reg)
                 sw = Saturations[1, c]
                 sg = Saturations[3, c]
-                Δp[1, c] = -pcow(sw)
-                Δp[2, c] = pcog(sg)
+                Δp[1, c] = -pcow_c(sw)
+                Δp[2, c] = pcog_c(sg)
             end
         end
     else
