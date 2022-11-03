b6c7c1cacbfda263c30df40edaac3dc8a8457e3b
Olav Møyner <olav.moyner@gmail.com>
2022-11-03 20:06:56 +0100

    Switch to TestItemRunner for VSCode CI integration

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,26 @@
-using JutulDarcy
-include("utils.jl")
-include("relperm.jl")
-include("singlephase.jl")
-include("multiphase.jl")
-include("multimodel.jl")
-include("sens_bl.jl")
-include("sens_multimodel.jl")
+using JutulDarcy, TestItems, TestItemRunner
+@testitem "Utilities" begin
+    include("utils.jl")
+end
+@testitem "Rel. Perm." begin
+    include("relperm.jl")
+end
+@testitem "Single-phase" begin
+    include("singlephase.jl")
+end
+@testitem "Multi-phase" begin
+    include("multiphase.jl")
+end
+@testitem "Multi-model (wells + reservoir)" begin
+    include("multimodel.jl")
+end
+@testitem "Sensitivities (simple)" begin
+    include("sens_bl.jl")
+end
+@testitem "Sensitivities (multimodel)" begin
+    include("sens_multimodel.jl")
+end
+
+@run_package_tests
+nothing
 # include("gpu.jl")
