9c48d8b892a7bca11c440db0dbbf49eedc58a829
Olav Møyner <olav.moyner@gmail.com>
2023-02-15 21:06:27 +0100

    Tweak well equations again

--- src/facility/wells/stdwells_equations.jl
+++ src/facility/wells/stdwells_equations.jl
@@ -4,24 +4,23 @@ function Jutul.update_equation_in_entity!(eq_buf::AbstractVector{T_e}, self_cell
     mass0 = state0.MassFractions
     p = state.Pressure[1]
     p0 = state0.Pressure[1]
-    ϵ = 1e-3
+    ϵ = 1e-20
     for i in eachindex(eq_buf)
         m = mass[i]
         m0 = mass0[i]
         if i == 1
-            Δ = (p - p0)*1e-18
+            # Make sure that no sources/sinks means that the pressure remains
+            # the same and mass fractions remain the same.
+            Δ = abs(p - p0)
         else
             Δ = 0
         end
-        eq_buf[i] = ϵ*(m - m0)/dt + Δ
+        eq_buf[i] = ϵ*(abs(m - m0) + Δ)
     end
 end
 
 function Jutul.convergence_criterion(model, storage, eq::SimpleWellEquation, eq_s, r; dt = 1)
-    # For CNV = 0.001 we assume 1% Inf error in well composition balance
-    ϵ = 0.01
     e = maximum(abs, r)
-    scale = dt/ϵ
-    R = (CNV = (errors = e*scale, names = "R"), )
+    R = (CNV = (errors = e, names = "R"), )
     return R
 end
