feefc52e9ce56ed5e057d469b1cb91af00944ea6
Olav Møyner <olav.moyner@gmail.com>
2023-02-21 13:54:38 +0100

    Blackoil specialized version of mass balance test

--- src/blackoil/blackoil.jl
+++ src/blackoil/blackoil.jl
@@ -56,3 +56,49 @@ has_disgas(::StandardBlackOilSystem) = true
 
 has_vapoil(::DisgasBlackOilSystem) = false
 has_disgas(::VapoilBlackOilSystem) = false
+
+function convergence_criterion(model::SimulationModel{D, S}, storage, eq::ConservationLaw, eq_s, r; dt = 1) where {D, S<:StandardBlackOilSystem}
+    M = global_map(model.domain)
+    v = x -> as_value(Jutul.active_view(x, M, for_variables = false))
+    Φ = v(storage.state.FluidVolume)
+    b = v(storage.state.ShrinkageFactors)
+
+    sys = model.system
+    nph = number_of_phases(sys)
+    rhoS = reference_densities(sys)
+    cnv, mb = cnv_mb_errors_bo(r, Φ, b, dt, rhoS, Val(nph))
+
+    names = phase_names(model.system)
+    R = (CNV = (errors = cnv, names = names),
+         MB = (errors = mb, names = names))
+    return R
+end
+
+function cnv_mb_errors_bo(r, Φ, b, dt, rhoS, ::Val{N}) where N
+    nc = length(Φ)
+    mb = @MVector zeros(N)
+    cnv = @MVector zeros(N)
+    avg_B = @MVector zeros(N)
+
+    pv_t = 0.0
+    @inbounds for c in 1:nc
+        pv_c = Φ[c]
+        pv_t += pv_c
+        @inbounds for ph = 1:N
+            r_ph = r[ph, c]
+            b_ph = b[ph, c]
+            # MB
+            mb[ph] += r_ph
+            avg_B[ph] += 1/b_ph
+            # CNV
+            cnv[ph] = max(cnv[ph], abs(r_ph)/pv_c)
+        end
+    end
+    @inbounds for ph = 1:N
+        B = avg_B[ph]/nc
+        scale = B*dt/rhoS[ph]
+        mb[ph] = scale*abs(mb[ph])/pv_t
+        cnv[ph] = scale*abs(cnv[ph])
+    end
+    return (Tuple(cnv), Tuple(mb))
+end
