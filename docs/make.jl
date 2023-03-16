d350952a570de82eea2dd093270da69fb6c2d5ed
Olav Møyner <olav.moyner@gmail.com>
2023-03-16 10:28:13 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,40 +3,49 @@ using Jutul
 using Literate
 using Documenter
 
-DocMeta.setdocmeta!(JutulDarcy, :DocTestSetup, :(using JutulDarcy; using Jutul); recursive=true)
-## Literate pass
-# Base directory
-jutul_dir = joinpath(dirname(pathof(JutulDarcy)), "..")
-# Convert examples as .jl files to markdown
-examples = ["Intro to wells" => "wells_intro"]
-examples_markdown = ["Getting started" => "examples/intro.md"]
-for (ex, pth) in examples
-    in_pth = joinpath(jutul_dir, "examples", "$pth.jl")
-    out_dir = joinpath(jutul_dir, "docs", "src", "examples")
-    push!(examples_markdown, ex => joinpath("examples", "$pth.md"))
-    Literate.markdown(in_pth, out_dir)
-end
-## Docs
-makedocs(;
-    modules=[JutulDarcy, Jutul],
-    authors="Olav Møyner <olav.moyner@sintef.no> and contributors",
-    repo="https://github.com/sintefmath/JutulDarcy.jl/blob/{commit}{path}#{line}",
-    sitename="JutulDarcy.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://sintefmath.github.io/JutulDarcy.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-        "Examples" => examples_markdown,
-        "Usage" => "usage.md",
-        "Internals" => "internals.md"
-    ],
-)
+function build_jutul_darcy_docs(build_format = nothing; build_examples = true)
+    DocMeta.setdocmeta!(JutulDarcy, :DocTestSetup, :(using JutulDarcy; using Jutul); recursive=true)
+    ## Literate pass
+    # Base directory
+    jutul_dir = joinpath(dirname(pathof(JutulDarcy)), "..")
+    # Convert examples as .jl files to markdown
+    examples = ["Intro to wells" => "wells_intro"]
+    examples_markdown = ["Getting started" => "examples/intro.md"]
+    if build_examples
+        for (ex, pth) in examples
+            in_pth = joinpath(jutul_dir, "examples", "$pth.jl")
+            out_dir = joinpath(jutul_dir, "docs", "src", "examples")
+            push!(examples_markdown, ex => joinpath("examples", "$pth.md"))
+            Literate.markdown(in_pth, out_dir)
+        end
+    end
+    ## Docs
+    if isnothing(build_format)
+        build_format = Documenter.HTML(;
+            prettyurls=get(ENV, "CI", "false") == "true",
+            canonical="https://sintefmath.github.io/JutulDarcy.jl",
+            edit_link="main",
+            assets=String[],
+        )
+    end
+    makedocs(;
+        modules=[JutulDarcy, Jutul],
+        authors="Olav Møyner <olav.moyner@sintef.no> and contributors",
+        repo="https://github.com/sintefmath/JutulDarcy.jl/blob/{commit}{path}#{line}",
+        sitename="JutulDarcy.jl",
+        format=build_format,
+        pages=[
+            "Home" => "index.md",
+            "Examples" => examples_markdown,
+            "Usage" => "usage.md",
+            "Internals" => "internals.md"
+        ],
+    )
 
-deploydocs(;
-    repo="github.com/sintefmath/JutulDarcy.jl",
-    devbranch="main",
-)
+    deploydocs(;
+        repo="github.com/sintefmath/JutulDarcy.jl",
+        devbranch="main",
+    )
+end
+
+build_jutul_darcy_docs()
