ffa3f5ef13bf238c2c9c30faa962ca621e128442
Olav Møyner <olav.moyner@gmail.com>
2023-03-31 15:58:13 +0200

    Update sequential.jl

--- src/formulations/sequential/sequential.jl
+++ src/formulations/sequential/sequential.jl
@@ -20,7 +20,30 @@ function SequentialSimulator(model; state0 = setup_state(model), parameters = se
     PSim = subsimulator(pmodel)
     TSim = subsimulator(tmodel)
     S = JutulStorage()
+    S[:recorder] = ProgressRecorder()
     return SequentialSimulator(model, PSim, TSim, S)
 end
+
+function Jutul.select_linear_solver(sim::SequentialSimulator; kwarg...)
+    return nothing
+end
+
+
+function Jutul.initial_setup!(sim::SequentialSimulator, config, timesteps; kwarg...)
+    Jutul.initial_setup!(sim.pressure, config, timesteps; kwarg...)
+end
+
+function Jutul.initialize_before_first_timestep!(sim::SequentialSimulator, dt; kwarg...)
+    Jutul.initialize_before_first_timestep!(sim.pressure, dt; kwarg...)
+end
+
+function Jutul.perform_step!(simulator::SequentialSimulator, dt, forces, config; kwarg...)
+    # Solve pressure
+    e_p, converged_p, report_p = Jutul.perform_step!(simulator.pressure, dt, forces, config; kwarg...)
+    # Copy over values for pressure and fluxes into parameters for second simulator
+    # Then transport
+    # Return convergence criterion for outer loop
+end
+
 
 include("interface.jl")
