ab5a6150c73cedaa8fc122fb722bb9e3c3acdfc2
Olav Møyner <olav.moyner@gmail.com>
2023-05-22 15:36:00 +0200

    Update cpr.jl

--- ext/JutulDarcyHYPREExt/cpr.jl
+++ ext/JutulDarcyHYPREExt/cpr.jl
@@ -16,36 +16,46 @@ function update_pressure_system_hypre!(single_buf, longer_buf, V_buffers, A_p, A
     @assert length(single_buf) == 1
     (; iupper, ilower) = A_p
     owned_num = iupper - ilower + 1 # 5
+    # @info "?!" owned_num iupper ilower n
     @assert n == iupper - ilower + 1 "$(n-1) != $ilower -> $iupper"
     offset = ilower - 1
 
     assembler = HYPRE.start_assemble!(A_p)
 
-    T = eltype(A)
+    @info "Assembly start"
     for row in 1:n
         pos_ix = nzrange(A, row)
         k = length(pos_ix)
         I = single_buf
-        I[1] = row + offset
+        I[1] = Jutul.executor_index_to_global(executor, row, :row)
         J = longer_buf
         resize!(J, k)
         V_buf = V_buffers[k]
+        num_added = 0
         @inbounds for ki in 1:k
             ri = pos_ix[ki]
+            col = cols[ri]
             A_block = nzval[ri]
             next_value = 0.0
             for component in axes(w_p, 1)
                 next_value += w_p[component, row]*A_block[component, 1]
             end
             V_buf[ki] = next_value
-            J[ki] = cols[ri] + offset
+            J[ki] = Jutul.executor_index_to_global(executor, col, :column)
+            num_added += 1
         end
+
+        @info "Assembling in" I (ilower, iupper)
+        @info "?? $num_added"  J
         HYPRE.assemble!(assembler, I, J, V_buf)
     end
     HYPRE.finish_assemble!(assembler)
+    @info "Assembly done"
 end
 
 function JutulDarcy.update_p_rhs!(r_p::HYPRE.HYPREVector, y, bz, w_p)
+    @info "RHS start"
+
     tmp = zeros(length(y)÷bz)
     for i in eachindex(tmp)
         v = 0.0
@@ -61,6 +71,7 @@ function JutulDarcy.update_p_rhs!(r_p::HYPRE.HYPREVector, y, bz, w_p)
     assembler = HYPRE.start_assemble!(r_p)
     HYPRE.assemble!(assembler, ix, tmp)
     HYPRE.finish_assemble!(assembler)
+    @info "RHS done" ix
 end
 
 function JutulDarcy.correct_residual_for_dp!(y, x, Δp::HYPRE.HYPREVector, bz, buf, A)
