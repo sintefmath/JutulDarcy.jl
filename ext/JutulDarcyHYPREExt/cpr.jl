5df9345116ba7112e4c48d60624e1e7e2f94fd84
Olav Møyner <olav.moyner@gmail.com>
2023-06-01 14:36:27 +0200

    Updates for distributed HYPRE

--- ext/JutulDarcyHYPREExt/cpr.jl
+++ ext/JutulDarcyHYPREExt/cpr.jl
@@ -103,6 +103,7 @@ end
 
 
 function Jutul.parray_update_preconditioners!(sim, cpr::CPRPreconditioner{<:BoomerAMGPreconditioner, <:Any}, preconditioners, recorder, tmr)
+    # @info "!!" sim.storage.nc sim.storage.nc_process sim.storage.process_offset
     offset = sim.storage.process_offset
     n = sim.storage.nc_process
     comm = sim.storage.comm
@@ -114,7 +115,6 @@ function Jutul.parray_update_preconditioners!(sim, cpr::CPRPreconditioner{<:Boom
         return x
     end
     if isnothing(cpr.A_p)
-        # @info "SETTING CPR START" n
         cpr.A_p = HYPREMatrix(comm, offset + 1, offset + n)
         cpr.r_p = create_hypre_vector()
         cpr.p = create_hypre_vector()
@@ -125,7 +125,7 @@ function Jutul.parray_update_preconditioners!(sim, cpr::CPRPreconditioner{<:Boom
     x_p = cpr.p
 
     map(sim.storage.simulators, preconditioners) do sim, prec
-        tic!(tmr)
+        # tic!(tmr)
         sys = sim.storage.LinearizedSystem
         model = sim.model
         storage = sim.storage
@@ -136,69 +136,9 @@ function Jutul.parray_update_preconditioners!(sim, cpr::CPRPreconditioner{<:Boom
 
         prec.pressure_precond.data[:hypre_system] = (A_p, r_p, x_p)
         Jutul.update_preconditioner!(prec, sys, model, storage, recorder, sim.executor)
-        toc!(tmr, "Update preconditioner")
+        # toc!(tmr, "Update preconditioner")
         prec
     end
 
     return (cpr, preconditioners)
-end
-
-function Jutul.parray_preconditioner_apply!(global_out, main_prec::CPRPreconditioner{<:BoomerAMGPreconditioner, <:Any}, X, preconditioners, simulator, arg...)
-    tmr = simulator.storage.global_timer
-    global_cell_vector = simulator.storage.distributed_cell_buffer
-    global_buf = simulator.storage.distributed_residual_buffer
-    tic!(tmr)
-    # toc!(tmr, "Apply preconditioner communication")
-    tic!(tmr)
-    map(local_values(X), preconditioners, ghost_values(X)) do x, prec, x_g
-        @. x_g = 0.0
-        JutulDarcy.apply_cpr_first_stage!(prec, x, arg...)
-        nothing
-    end
-    toc!(tmr, "Apply CPR stage 1")
-    tic!(tmr)
-    # The following is an unsafe version of this:
-    # copy!(global_cell_vector, main_prec.p)
-    map(own_values(global_cell_vector), preconditioners) do ov, prec
-        helper = prec.pressure_precond.data[:assembly_helper]
-        indices = helper.indices
-        indices::Vector{HYPRE.HYPRE_BigInt}
-        nvalues = indices[end] - indices[1] + 1
-        HYPRE.@check HYPRE.HYPRE_IJVectorGetValues(main_prec.p, nvalues, indices, ov)
-    end
-    # End unsafe shenanigans
-
-    # consistent!(global_cell_vector) |> wait
-    toc!(tmr, "Apply preconditioner communication")
-    tic!(tmr)
-
-    map(own_values(global_buf), own_values(global_cell_vector), preconditioners) do dx, dp, prec
-        bz = prec.block_size
-        for i in eachindex(dp)
-            JutulDarcy.set_dp!(dx, bz, dp, i)
-        end
-        nothing
-    end
-    n = length(global_buf)
-    if main_prec.full_system_correction
-        mul_ix = nothing
-    else
-        mul_ix = 1
-    end
-    distributed_mul! = setup_distributed_mul!(tmr, simulator.storage.simulators, mul_ix)
-    full_op = LinearOperator(Float64, n, n, false, false, distributed_mul!)
-    mul!(X, full_op, global_buf, -1.0, true)
-
-    map(local_values(global_out), local_values(X), preconditioners, local_values(global_cell_vector), ghost_values(X)) do y, x, prec, dp, x_g
-        @. x_g = 0.0
-        apply!(y, prec.system_precond, x, arg...)
-        bz = prec.block_size
-        JutulDarcy.increment_pressure!(y, dp, bz)
-        nothing
-    end
-    toc!(tmr, "CPR second stage")
-    tic!(tmr)
-    consistent!(global_out) |> wait
-    toc!(tmr, "Apply preconditioner communication")
-    global_out
 end
--- ext/JutulDarcyPartitionedArraysExt/JutulDarcyPartitionedArraysExt.jl
+++ ext/JutulDarcyPartitionedArraysExt/JutulDarcyPartitionedArraysExt.jl
@@ -1,9 +1,8 @@
 module JutulDarcyPartitionedArraysExt
     using Jutul, JutulDarcy
     # Specific dependencies
-    using PartitionedArrays, MPI
-    # Already in Jutul
-    # using SparseArrays, Krylov, LinearAlgebra#, LinearOperators
+    using PartitionedArrays, MPI, HYPRE
+    using LinearAlgebra
 
     import Jutul: PArraySimulator, MPISimulator, PArrayExecutor
     import Jutul: DebugPArrayBackend, JuliaPArrayBackend, MPI_PArrayBackend
@@ -17,6 +16,67 @@ module JutulDarcyPartitionedArraysExt
         p_num = partition_distributed(N, T, nc = nc, np = np, groups = groups)
         p = reservoir_partition(case.model, p_num)
         return simulate_parray(case, p, backend)
+    end
+
+    function Jutul.parray_preconditioner_apply!(global_out, main_prec::CPRPreconditioner{<:BoomerAMGPreconditioner, <:Any}, X, preconditioners, simulator, arg...)
+        tmr = simulator.storage.global_timer
+        global_cell_vector = simulator.storage.distributed_cell_buffer
+        global_buf = simulator.storage.distributed_residual_buffer
+        # tic!(tmr)
+        # toc!(tmr, "Apply preconditioner communication")
+        # tic!(tmr)
+        map(local_values(X), preconditioners, ghost_values(X)) do x, prec, x_g
+            @. x_g = 0.0
+            JutulDarcy.apply_cpr_first_stage!(prec, x, arg...)
+            nothing
+        end
+        # toc!(tmr, "Apply CPR stage 1")
+        # tic!(tmr)
+        # The following is an unsafe version of this:
+        # copy!(global_cell_vector, main_prec.p)
+        map(own_values(global_cell_vector), preconditioners) do ov, prec
+            helper = prec.pressure_precond.data[:assembly_helper]
+            indices = helper.indices
+            indices::Vector{HYPRE.HYPRE_BigInt}
+            nvalues = indices[end] - indices[1] + 1
+            HYPRE.@check HYPRE.HYPRE_IJVectorGetValues(main_prec.p, nvalues, indices, ov)
+        end
+        # End unsafe shenanigans
+
+        # consistent!(global_cell_vector) |> wait
+        # toc!(tmr, "Apply preconditioner communication")
+        # tic!(tmr)
+
+        map(own_values(global_buf), own_values(global_cell_vector), preconditioners) do dx, dp, prec
+            bz = prec.block_size
+            for i in eachindex(dp)
+                JutulDarcy.set_dp!(dx, bz, dp, i)
+            end
+            nothing
+        end
+        # n = length(global_buf)
+        if main_prec.full_system_correction
+            mul_ix = nothing
+        else
+            mul_ix = 1
+        end
+        # distributed_mul! = Jutul.setup_parray_mul!(tmr, simulator.storage.simulators, mul_ix)
+        full_op = Jutul.parray_linear_system_operator(tmr, simulator.storage.simulators, global_buf)
+        # full_op = LinearOperator(Float64, n, n, false, false, distributed_mul!)
+        mul!(X, full_op, global_buf, -1.0, true)
+
+        map(local_values(global_out), local_values(X), preconditioners, local_values(global_cell_vector), ghost_values(X)) do y, x, prec, dp, x_g
+            @. x_g = 0.0
+            apply!(y, prec.system_precond, x, arg...)
+            bz = prec.block_size
+            JutulDarcy.increment_pressure!(y, dp, bz)
+            nothing
+        end
+        # toc!(tmr, "CPR second stage")
+        # tic!(tmr)
+        consistent!(global_out) |> wait
+        # toc!(tmr, "Apply preconditioner communication")
+        global_out
     end
 
 end
