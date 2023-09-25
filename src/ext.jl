d4d03be99d629f365930e78191dfcc003b7dff66
Olav Møyner <olav.moyner@gmail.com>
2023-09-25 14:45:29 +0200

    Add plotting function

--- src/ext.jl
+++ src/ext.jl
@@ -40,6 +40,21 @@ function plot_reservoir_simulation_result(model::MultiModel, res::ReservoirSimRe
     return fig
 end
 
+export plot_reservoir
+function plot_reservoir(model, arg...; kwarg...)
+    rmodel = reservoir_model(model)
+    fig = plot_interactive(rmodel, arg...; kwarg...)
+    g = physical_representation(rmodel.data_domain)
+    ax = fig.current_axis[]
+    for (k, m) in pairs(model.models)
+        w = physical_representation(m.data_domain)
+        if w isa WellDomain
+            plot_well!(ax, g, w)
+        end
+    end
+    return fig
+end
+
 export simulate_reservoir_parray
 function simulate_reservoir_parray(case, mode = :mpi; kwarg...)
     sim, cfg = setup_reservoir_simulator(case; mode = mode, kwarg...)
