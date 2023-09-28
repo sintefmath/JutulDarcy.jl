4534f38528415d10cd14994bc35ce2eac8b34008
Olav Møyner <olav.moyner@gmail.com>
2023-09-28 10:50:41 +0200

    Rewrite compositional criterion
    Fix issue of no scaling with immiscible weight, cleanup

--- src/multicomponent/multicomponent.jl
+++ src/multicomponent/multicomponent.jl
@@ -129,20 +129,28 @@ function immiscible_increment(model, state, update_report)
 end
 
 function compositional_residual_scale(cell, dt, w, sl, liquid_density, sv, vapor_density, sw, water_density, vol)
-    function sat_scale(::Nothing)
-        return 1.0
-    end
-    function sat_scale(sw)
+    if isnothing(sw)
+        sw_i = 0.0
+    else
         sw_i = sw[cell]
-        if sw_i > 1.0 - 1e-4
-            scale = 0.0
-        else
-            scale = 1.0 - sw_i
-        end
-        return scale
     end
-    total_density = liquid_density[cell] * sl[cell] + vapor_density[cell] * sv[cell]
-    return dt * mean(w) * (sat_scale(sw) / (vol[cell] * max(total_density, 1e-3)))
+
+    if sw_i > 1.0 - 1e-4
+        scale = 0.0
+    else
+        # The convergence criterion is taken to be dimensionless in a similar
+        # manner to the standard CNV type criterion. We scale everything by the
+        # immiscible phase so that the convergence criterion is more relaxed in
+        # cells that are close to or completely filled with the immiscible
+        # phase.
+        scale_lv = 1.0 - sw_i
+        sl_scaled = sl[cell]/scale_lv
+        denl = liquid_density[cell]
+        denv = vapor_density[cell]
+        total_density = denl * sl_scaled + denv * (1.0 - sl_scaled)
+        scale = dt * mean(w) * (scale_lv / (vol[cell] * max(total_density, 1e-3)))
+    end
+    return scale
 end
 
 function compositional_criterion(state, dt, active, r, nc, w, sl, liquid_density, sv, vapor_density, sw, water_density, vol)
