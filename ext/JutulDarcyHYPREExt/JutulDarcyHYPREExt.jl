a42ff9c8804a47fbeb183381378b0cafff849a72
Olav Møyner <olav.moyner@gmail.com>
2023-10-18 15:39:03 +0200

    Update JutulDarcyHYPREExt.jl
    Disable precompilation for extension

--- ext/JutulDarcyHYPREExt/JutulDarcyHYPREExt.jl
+++ ext/JutulDarcyHYPREExt/JutulDarcyHYPREExt.jl
@@ -6,13 +6,13 @@ module JutulDarcyHYPREExt
     using PrecompileTools
     include("cpr.jl")
 
-    @compile_workload begin
-        targets = [(true, :csc), (true, :csr)]
-        # MPI, trivial partition
-        JutulDarcy.precompile_darcy_multimodels(targets,
-            dims = (4, 1, 1),
-            precond = :cpr,
-            amg_type = :hypre
-        )
-    end
+    # @compile_workload begin
+    #     targets = [(true, :csc), (true, :csr)]
+    #     # MPI, trivial partition
+    #     JutulDarcy.precompile_darcy_multimodels(targets,
+    #         dims = (4, 1, 1),
+    #         precond = :cpr,
+    #         amg_type = :hypre
+    #     )
+    # end
 end
