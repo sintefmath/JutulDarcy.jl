ce7e23bb6d57b00670cac1a3b344a91a74eb2b2c
Olav Møyner <olav.moyner@gmail.com>
2023-11-26 11:05:41 +0100

    Reduce default pressure scale to 1 bar

--- src/multiphase.jl
+++ src/multiphase.jl
@@ -91,8 +91,8 @@ absolute/relative change over a Newton iteration, `scale` is a "typical" value
 used to regularize the linear system, `maximum` the largest possible value and
 `minimum` the smallest.
 """
-function Pressure(; max_abs = nothing, max_rel = 0.2, scale = 1e8, maximum = Inf, minimum = DEFAULT_MINIMUM_PRESSURE)
-    Pressure(max_abs, max_rel, minimum, maximum, scale)
+function Pressure(; max_abs = nothing, max_rel = 0.2, scale = si_unit(:bar), maximum = Inf, minimum = DEFAULT_MINIMUM_PRESSURE)
+    return Pressure(max_abs, max_rel, minimum, maximum, scale)
 end
 
 Jutul.variable_scale(p::Pressure) = p.scale
