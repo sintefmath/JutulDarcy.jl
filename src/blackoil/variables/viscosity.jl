fe70d40417fabba451cfef863d0c669095d32a48
Olav Møyner <olav.moyner@gmail.com>
2024-01-12 10:33:40 +0100

    Fix viscosity for disgas without water

--- src/blackoil/variables/viscosity.jl
+++ src/blackoil/variables/viscosity.jl
@@ -1,7 +1,12 @@
 @jutul_secondary function update_deck_viscosity!(μ, ρ::DeckPhaseViscosities, model::StandardBlackOilModel, Pressure, Rs, Rv, ix)
     pvt, reg = ρ.pvt, ρ.regions
-    w, o, g = phase_indices(model.system)
-    muW = pvt[w]
+    has_wat = has_other_phase(model.system)
+    if has_wat
+        w, o, g = phase_indices(model.system)
+        muW = pvt[w]
+    else
+        o, g = phase_indices(model.system)
+    end
     muO = pvt[o]
     muG = pvt[g]
 
@@ -9,7 +14,9 @@
         p = Pressure[i]
         rs = Rs[i]
         rv = Rv[i]
-        μ[w, i] = viscosity(muW, reg, p, i)
+        if has_wat
+            μ[w, i] = viscosity(muW, reg, p, i)
+        end
         μ[o, i] = viscosity(muO, reg, p, rs, i)
         μ[g, i] = viscosity(muG, reg, p, rv, i)
     end
@@ -17,15 +24,22 @@ end
 
 @jutul_secondary function update_deck_viscosity!(μ, ρ::DeckPhaseViscosities, model::VapoilBlackOilModel, Pressure, Rv, ix)
     pvt, reg = ρ.pvt, ρ.regions
-    w, o, g = phase_indices(model.system)
-    muW = pvt[w]
+    has_wat = has_other_phase(model.system)
+    if has_wat
+        w, o, g = phase_indices(model.system)
+        muW = pvt[w]
+    else
+        o, g = phase_indices(model.system)
+    end
     muO = pvt[o]
     muG = pvt[g]
 
     @inbounds for i in ix
         p = Pressure[i]
         rv = Rv[i]
-        μ[w, i] = viscosity(muW, reg, p, i)
+        if has_wat
+            μ[w, i] = viscosity(muW, reg, p, i)
+        end
         μ[o, i] = viscosity(muO, reg, p, i)
         μ[g, i] = viscosity(muG, reg, p, rv, i)
     end
