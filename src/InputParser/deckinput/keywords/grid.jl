a48e7780a191906c311890d50842793958333b97
Olav Møyner <olav.moyner@gmail.com>
2024-01-25 10:23:37 +0100

    Update grid.jl

--- src/InputParser/deckinput/keywords/grid.jl
+++ src/InputParser/deckinput/keywords/grid.jl
@@ -59,9 +59,33 @@ function parse_keyword!(data, outer_data, units, cfg, f, ::Val{:GRIDUNIT})
     partial_parse!(data, outer_data, units, cfg, f, :GRIDUNIT)
 end
 
+function check_unit(unit_str, units, kw)
+    ref = uppercase("$(deck_unit_system_label(units.from))")
+    u = uppercase(unit_str)
+    if u != ref
+        @warn "Unit mismatch in $kw: Was $u but RUNSPEC declared $ref"
+    end
+end
+
 function parse_keyword!(data, outer_data, units, cfg, f, ::Val{:FILEUNIT})
-    # TODO: This needs to be handled
-    partial_parse!(data, outer_data, units, cfg, f, :FILEUNIT)
+    rec = strip(only(read_record(f)))
+    check_unit(rec, units, "FILEUNIT")
+    data["FILEUNIT"] = rec
+end
+
+function parse_keyword!(data, outer_data, units, cfg, f, ::Val{:GRIDUNIT})
+    rec = read_record(f)
+    tdims = ["Default", "MAP"]
+    v = parse_defaulted_line(rec, tdims)
+    check_unit(v[1], units, "GRIDUNIT")
+    data["GRIDUNIT"] = v
+end
+
+function parse_keyword!(data, outer_data, units, cfg, f, ::Val{:MAPUNITS})
+    rec = read_record(f)
+    tdims = ["Default"]
+    v = parse_defaulted_line(rec, tdims)
+    data["MAPUNITS"] = only(v)
 end
 
 function parse_keyword!(data, outer_data, units, cfg, f, ::Val{:GDORIENT})
