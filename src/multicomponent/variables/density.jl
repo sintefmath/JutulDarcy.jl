a2996d450533f3745226bbd275faaaa42eb733c9
Olav Møyner <olav.moyner@gmail.com>
2024-02-12 15:36:42 +0100

    Add two variable defs for CO2 models

--- src/multicomponent/variables/density.jl
+++ src/multicomponent/variables/density.jl
@@ -32,3 +32,42 @@ end
         rho[a, i] = rhos[a]*shrinkage(pvt, p)
     end
 end
+
+struct BrineCO2MixingDensities{T} <: AbstractCompositionalDensities
+    tab::T
+    coeffs::NTuple{4, Float64}
+end
+
+function BrineCO2MixingDensities(tab::T; coefficients = (37.51, −9.585*10e−2, 8.74*10e−4, −5.044*10e−7)) where T
+    return BrineCO2MixingDensities{T}(tab, coefficients)
+end
+
+@jutul_secondary function update_density!(rho, rho_def::BrineCO2MixingDensities, model::SimulationModel{D, S}, Pressure, Temperature, LiquidMassFractions, ix) where {D, S<:CompositionalSystem}
+    # V = 10e−6*(37.51 − 9.585*10e−2*T + 8.74*10e−4*T^2 − 5.044*10e−7*T^3)
+    # M_co2 = 44.01*10^-3
+    # ρ_co2 = M_co2/V
+
+    c1, c2, c3, c4 = rho_def.coeffs
+    sys = model.system
+    eos = sys.equation_of_state
+    cnames = eos.mixture.component_names
+    # TODO: This is hard coded.
+    @assert cnames[1] == raw"CarbonDioxide"
+    @assert length(cnames) == 2
+    l, v = phase_indices(sys)
+    @inbounds for i in ix
+        p = Pressure[i]
+        T = Temperature[i]
+        X_co2 = LiquidMassFractions[1, i]
+        X_h2o = LiquidMassFractions[2, i]
+
+        rho_co2, rho_h2o_pure = rho_def.tab(p, T)
+
+        vol_co2 = 10e−6*(c1 + c2*T + c3*T^2 + c4*T^3)
+        rho_liquid_co2_pure = 44.01*10^-3/vol_co2
+        rho_brine_mix = rho_liquid_co2_pure*X_co2 + rho_h2o_pure*X_h2o
+
+        rho[v, i] = rho_co2
+        rho[l, i] = rho_brine_mix
+    end
+end
--- src/multicomponent/variables/saturations.jl
+++ src/multicomponent/variables/saturations.jl
@@ -32,3 +32,41 @@ end
     end
 end
 
+struct SaturationsFromDensities <: PhaseVariables
+
+end
+
+
+@jutul_secondary function update_saturations!(Sat, s::SaturationsFromDensities, model::SimulationModel{D, S}, PhaseMassDensities, FlashResults, ix) where {D, S<:MultiPhaseCompositionalSystemLV{E, T, O} where {E, T, O<:Nothing}}
+    l, v = phase_indices(model.system)
+    @assert number_of_phases(model.system) == 2
+    eos = model.system.equation_of_state
+    props = map(x -> x.mw, eos.mixture.properties)
+    @inbounds for i in ix
+        flash = FlashResults[i]
+        x = flash.liquid.mole_fractions
+        y = flash.vapor.mole_fractions
+        if flash.state == MultiComponentFlash.two_phase_lv
+            # Calculate values that are proportional to volume and get
+            # saturation from that.
+            mass_liquid = 0.0
+            mass_vapor = 0.0
+            for c in eachindex(props)
+                mw = props[i].mw
+                mass_liquid += mw*x[i]
+                mass_vapor += mw*y[i]
+            end
+            rho_l = PhaseMassDensities[l, i]
+            rho_l = PhaseMassDensities[v, i]
+            vol_liquid = mass_liquid/rho_l
+            vol_vapor = mass_vapor/rho_v
+            S_v = vol_vapor/(vol_liquid + vol_vapor)
+        elseif flash.state == MultiComponentFlash.single_phase_v
+            S_v = 1.0
+        else
+            S_v = 0.0
+        end
+        Sat[v, i] = S_v
+        Sat[l, i] = 1.0 - S_v
+    end
+end
