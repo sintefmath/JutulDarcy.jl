77fb9c80e8af0af65178d25e77244c259e16dfe8
Olav Møyner <olav.moyner@gmail.com>
2024-02-13 19:37:49 +0100

    Add KValueWrapper type

--- src/multicomponent/utils.jl
+++ src/multicomponent/utils.jl
@@ -16,3 +16,42 @@ has_other_phase(sys::MultiPhaseCompositionalSystemLV{E, T, O}) where {E, T, O<:N
 
 phase_indices(sys::MultiPhaseCompositionalSystemLV{E, T, O}) where {E, T, O<:Nothing} = (liquid_phase_index(sys), vapor_phase_index(sys))
 phase_indices(sys::MultiComponentSystem) = (other_phase_index(sys), liquid_phase_index(sys), vapor_phase_index(sys))
+
+export KValueWrapper
+struct KValueWrapper{T, D}
+    K::T
+end
+
+"""
+    KValueWrapper(K; dependence::Symbol = :pT)
+
+Create a wrapper for a K-value interpolator to be used with K-value flash.
+
+The main purpose of this wrapper is to transform the general flash cond
+NamedTuple into the right arguments for multi-linear interpolation.
+"""
+function KValueWrapper(K::T; dependence::Symbol = :pT) where T
+    choices = (:p, :T, :pT, :pTz)
+    dependence in choices || throw(ArgumentError("Bad input: dependence = $dependence was not in $choices"))
+    return KValueWrapper{T, dependence}(K)
+end
+
+function (k::KValueWrapper{<:Any, :p})(cond::NamedTuple)
+    val = k.K(cond.p)
+    return val
+end
+
+function (k::KValueWrapper{<:Any, :T})(cond::NamedTuple)
+    val = k.K(cond.T)
+    return val
+end
+
+function (k::KValueWrapper{<:Any, :pT})(cond::NamedTuple)
+    val = k.K(cond.p, cond.T)
+    return val
+end
+
+function (k::KValueWrapper{<:Any, :T})(cond::NamedTuple)
+    val = k.K(cond.p, cond.T, cond.z)
+    return val
+end
