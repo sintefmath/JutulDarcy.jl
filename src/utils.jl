6475e97920122a99b848d76ba9189bdcbc496e4c
Olav Møyner <olav.moyner@gmail.com>
2024-02-26 13:56:41 +0100

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -493,7 +493,7 @@ function set_default_cnv_mb_inner!(tol, model;
         inc_tol_dz = Inf
         )
     sys = model.system
-    if model isa Jutul.CompositeModel && hasproperty(model.system, :flow)
+    if model isa Jutul.CompositeModel && hasproperty(model.system.systems, :flow)
         sys = flow_system(model.system)
     end
     if sys isa ImmiscibleSystem || sys isa BlackOilSystem || sys isa CompositionalSystem
