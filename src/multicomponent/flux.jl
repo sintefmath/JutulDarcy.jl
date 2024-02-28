448e8cd831b8b8e0db16c58b4fffe964577f5f03
Olav Møyner <olav.moyner@gmail.com>
2024-02-28 22:23:12 +0100

    Add diffusion to compositional flux

--- src/multicomponent/flux.jl
+++ src/multicomponent/flux.jl
@@ -5,41 +5,73 @@
 
     X = state.LiquidMassFractions
     Y = state.VaporMassFractions
+    S = state.Saturations
+    ρ = state.PhaseMassDensities
+    if haskey(state, :Diffusivities)
+        D = state.Diffusivities
+    else
+        D = nothing
+    end
     kdisc = flux_primitives(face, state, model, flux_type, kgrad, upw)
-    q = compositional_fluxes!(q, face, state, X, Y, model, flux_type, kgrad, kdisc, upw, aqua, ph_ix)
+    q = compositional_fluxes!(q, face, state, S, ρ, X, Y, D, model, flux_type, kgrad, kdisc, upw, aqua, ph_ix)
     return q
 end
 
-@inline function compositional_fluxes!(q, face, state, X, Y, model, flux_type, kgrad, kdisc, upw, aqua::Val{false}, phase_ix)
+@inline function compositional_fluxes!(q, face, state, S, ρ, X, Y, D, model, flux_type, kgrad, kdisc, upw, aqua::Val{false}, phase_ix)
     nc = size(X, 1)
     l, v = phase_ix
     q_l = darcy_phase_mass_flux(face, l, state, model, flux_type, kgrad, upw, kdisc)
     q_v = darcy_phase_mass_flux(face, v, state, model, flux_type, kgrad, upw, kdisc)
 
-    q = inner_compositional!(q, X, Y, q_l, q_v, upw, nc)
+    q = inner_compositional!(q, S, ρ, X, Y, D, q_l, q_v, face, kgrad, upw, nc, phase_ix)
     return q
 end
 
-@inline function compositional_fluxes!(q, face, state, X, Y, model, flux_type, kgrad, kdisc, upw, aqua::Val{true}, phase_ix)
+@inline function compositional_fluxes!(q, face, state, S, ρ, X, Y, D, model, flux_type, kgrad, kdisc, upw, aqua::Val{true}, phase_ix)
     nc = size(X, 1)
     a, l, v = phase_ix
     q_a = darcy_phase_mass_flux(face, a, state, model, flux_type, kgrad, upw, kdisc)
     q_l = darcy_phase_mass_flux(face, l, state, model, flux_type, kgrad, upw, kdisc)
     q_v = darcy_phase_mass_flux(face, v, state, model, flux_type, kgrad, upw, kdisc)
 
-    q = inner_compositional!(q, X, Y, q_l, q_v, upw, nc)
+    q = inner_compositional!(q, S, ρ, X, Y, D, q_l, q_v, face, kgrad, upw, nc, (l, v))
     q = setindex(q, q_a, nc+1)
     return q
 end
 
-@inline function inner_compositional!(q, X, Y, q_l, q_v, upw, nc)
+@inline function inner_compositional!(q, S, ρ, X, Y, D, q_l, q_v, face, grad, upw, nc, lv)
     for i in 1:nc
         X_f = upwind(upw, cell -> @inbounds(X[i, cell]), q_l)
         Y_f = upwind(upw, cell -> @inbounds(Y[i, cell]), q_v)
-
         q_i = q_l*X_f + q_v*Y_f
+        q = setindex(q, q_i, i)
+    end
+    q = add_diffusive_component_flux(q, S, ρ, X, Y, D, face, grad, lv)
+    return q
+end
+
+function add_diffusive_component_flux(q, S, ρ, X, Y, D::Nothing, face, grad, lv)
+    return q
+end
+
+function add_diffusive_component_flux(q, S, ρ, X, Y, D, face, grad, lv)
+    l, v = lv
+    diff_mass_l = phase_diffused_mass(D, ρ, l, S, face, grad)
+    diff_mass_v = phase_diffused_mass(D, ρ, v, S, face, grad)
+
+    @inbounds for i in eachindex(q)
+        q_i = q[i]
+        dX = gradient(X, l, grad)
+        dY = gradient(Y, l, grad)
+
+        q_i += diff_mass_l*dX + diff_mass_v*dY
         q = setindex(q, q_i, i)
     end
     return q
 end
 
+function phase_diffused_mass(D, ρ, α, S, face, grad)
+    @inbounds D_α = D[α, face]
+    mass_α = cell -> @inbounds ρ[α, cell]*S[α, cell]
+    return -D_α*face_average(mass_α, grad)
+end
