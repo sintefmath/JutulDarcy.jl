60abffbab0b2ebdb5d9858a09dbbfc6e0e082ea7
Olav Møyner <olav.moyner@gmail.com>
2024-05-29 21:01:27 +0200

    Reopen closed wells within same report step if possible

--- src/facility/controls.jl
+++ src/facility/controls.jl
@@ -28,8 +28,9 @@ function Jutul.update_before_step_multimodel!(storage_g, model_g::MultiModel, mo
         newctrl, changed = realize_control_for_reservoir(rstate, forces.control[key], rmodel, dt)
         oldctrl = req_ctrls[key]
         is_new_step = cfg.step_index != current_step
-        well_was_disabled = op_ctrls[key] == DisabledControl()
-        if is_new_step && (newctrl != oldctrl || well_was_disabled)
+        disabled = DisabledControl()
+        well_was_disabled = op_ctrls[key] == disabled && newctrl != disabled
+        if (is_new_step && newctrl != oldctrl) || well_was_disabled
             # We have a new control. Any previous control change is invalid.
             # Set both operating and requested control to the new one.
             @debug "Well $key switching from $oldctrl to $newctrl"
