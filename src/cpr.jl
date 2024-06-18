9c4e1cc99997c2705929c8d604c6a1c273d2ef1f
Olav Møyner <olav.moyner@gmail.com>
2024-06-18 13:34:12 +0200

    Slight cleanup of cpr.jl

--- src/cpr.jl
+++ src/cpr.jl
@@ -380,7 +380,8 @@ function update_weights!(cpr, cpr_storage::CPRStorage, model, res_storage, J, ps
     r = cpr_storage.w_rhs
     ncomp = size(w, 1)
     scaling = cpr.weight_scaling
-    if cpr.strategy == :true_impes
+    strategy = cpr.strategy
+    if strategy == :true_impes
         eq_s = res_storage.equations[:mass_conservation]
         if eq_s isa ConservationLawTPFAStorage
             acc = eq_s.accumulation.entries
@@ -390,15 +391,15 @@ function update_weights!(cpr, cpr_storage::CPRStorage, model, res_storage, J, ps
             ps = 1.0
         end
         true_impes!(w, acc, r, n, ncomp, ps, scaling)
-    elseif cpr.strategy == :analytical
+    elseif strategy == :analytical
         rstate = res_storage.state
         cpr_weights_no_partials!(w, model, rstate, r, n, ncomp, scaling)
-    elseif cpr.strategy == :quasi_impes
+    elseif strategy == :quasi_impes
         quasi_impes!(w, J, r, n, ncomp, scaling)
-    elseif cpr.strategy == :none
+    elseif strategy == :none
         # Do nothing. Already set to one.
     else
-        error("Unsupported strategy $(cpr.strategy)")
+        error("Unsupported strategy $(strategy)")
     end
     return w
 end
