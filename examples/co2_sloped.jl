74b8a34300b1e605e737b504b89e23cf0c341141
Olav Møyner <olav.moyner@gmail.com>
2024-08-31 12:13:50 +0200

    Rewrite example

--- examples/co2_sloped.jl
+++ examples/co2_sloped.jl
@@ -24,7 +24,8 @@ for (i, pt) in enumerate(points)
     x, y, z = pt
     x_u = 2*π*x/1000.0
     w = 0.2
-    dz = 0.05*x + w*(30*sin(2.0*x_u) + 20*sin(5.0*x_u) + 10*sin(10.0*x_u) + 5*sin(25.0*x_u))
+    dz = 0.05*x + 0.05*abs(x - 500.0)+ w*(30*sin(2.0*x_u) + 20*sin(5.0*x_u))# + 10*sin(10.0*x_u) + 5*sin(25.0*x_u))
+    # dz = 0.05*x + w*(30*sin(2.0*x_u) + 20*sin(5.0*x_u) + 10*sin(10.0*x_u) + 5*sin(25.0*x_u))
     points[i] = pt + [0, 0, dz]
 end
 # ## Set up simulation model
@@ -34,20 +35,33 @@ end
 # appropriate functions for density, viscosity and miscibility.
 #
 # Note that this model can be run with a thermal mode by setting 
-domain = reservoir_domain(mesh, permeability = 1.0Darcy, porosity = 0.3, temperature = convert_to_si(30.0, :Celsius))
+domain = reservoir_domain(mesh, permeability = 0.3Darcy, porosity = 0.3, temperature = convert_to_si(30.0, :Celsius))
 Injector = setup_well(domain, (65, 1, 1), name = :Injector)
 model, parameters = setup_reservoir_model(domain, :co2brine, wells = Injector);
-# ## Find the boundary and set increased volume
-# We find the left and right boundary of the model and increase the volume of
-# those cells. This mimicks a constant pressure boundary condition.
+# ## Define approximate hydrostatic pressure
+# The initial pressure of the water-filled domain is assumed to be at
+# hydrostatic equilibrium.
+nc = number_of_cells(mesh)
+p0 = zeros(nc)
+depth = domain[:cell_centroids][3, :]
+g = Jutul.gravity_constant
+@. p0 = 200bar + depth*g*1000.0
+
+# ## Find the boundary and apply a constant pressureboundary condition
+# We find cells on the left and right boundary of the model and set a constant
+# pressure boundary condition to represent a bounding aquifer that retains the
+# initial pressure far away from injection.
+
 boundary = Int[]
-for cell in 1:number_of_cells(mesh)
+for cell in 1:nc
     I, J, K = cell_ijk(mesh, cell)
     if I == 1 || I == nx
         push!(boundary, cell)
     end
 end
-parameters[:Reservoir][:FluidVolume][boundary] *= 1000;
+bc = flow_boundary_condition(boundary, domain, p0[boundary], fractional_flow = [1.0, 0.0])
+
+
 # ## Plot the model
 plot_reservoir(model)
 # ## Set up schedule
@@ -58,7 +72,7 @@ nstep = 25
 nstep_shut = 25
 dt_inject = fill(365.0day, nstep)
 pv = pore_volume(model, parameters)
-inj_rate = 0.0075*sum(pv)/sum(dt_inject)
+inj_rate = 0.05*sum(pv)/sum(dt_inject)
 
 rate_target = TotalRateTarget(inj_rate)
 I_ctrl = InjectorControl(rate_target, [0.0, 1.0],
@@ -66,16 +80,19 @@ I_ctrl = InjectorControl(rate_target, [0.0, 1.0],
 )
 # Set up forces for use in injection
 controls = Dict(:Injector => I_ctrl)
-forces_inject = setup_reservoir_forces(model, control = controls)
+forces_inject = setup_reservoir_forces(model, control = controls, bc = bc)
 # Forces with shut wells
-forces_shut = setup_reservoir_forces(model)
+forces_shut = setup_reservoir_forces(model, bc = bc)
 dt_shut = fill(365.0day, nstep_shut);
 # Combine the report steps and forces into vectors of equal length
 dt = vcat(dt_inject, dt_shut)
-forces = vcat(fill(forces_inject, nstep), fill(forces_shut, nstep_shut));
+forces = vcat(
+    fill(forces_inject, nstep),
+    fill(forces_shut, nstep_shut)
+);
 # ## Set up initial state
 state0 = setup_reservoir_state(model,
-    Pressure = 200bar,
+    Pressure = p0,
     OverallMoleFractions = [1.0, 0.0],
 )
 # ## Simulate the schedule
@@ -84,7 +101,7 @@ state0 = setup_reservoir_state(model,
 wd, states, t = simulate_reservoir(state0, model, dt,
     parameters = parameters,
     forces = forces,
-    max_timestep = 30day
+    max_timestep = 90day
 )
 # ## Plot the density of brine
 # The density of brine depends on the CO2 concentration and gives a good
@@ -102,7 +119,7 @@ function plot_co2!(fig, ix, x, title = "")
 end
 fig = Figure(size = (900, 1200))
 for (i, step) in enumerate([1, 5, nstep, nstep+nstep_shut])
-    plot_co2!(fig, i, states[step][:PhaseMassDensities][1, :], "Brine density report step $step/$(nstep+nstep_shut)")
+    plot_co2!(fig, i, log10.(states[step][:OverallMoleFractions][2, :]), "log10 of CO2 mole fraction at report step $step/$(nstep+nstep_shut)")
 end
 fig
 # ## Plot result in interactive viewer
