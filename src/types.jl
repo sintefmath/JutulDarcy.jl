6876b13fcf3f52b792ed4175ce83af50737b22b7
Olav Møyner <olav.moyner@gmail.com>
2024-09-12 20:17:03 +0200

    Add doctest to kr

--- src/types.jl
+++ src/types.jl
@@ -264,7 +264,21 @@ equal length `s` and `k`):
 ``K_r = K(S)``
 
 Optionally, a label for the phase, the connate saturation and a small epsilon
-value used to avoid extrapolation can be specified.
+value used to avoid extrapolation can be specified. The return type holds both
+the table, the phase context, the autodetected critical and maximum relative
+permeability values and can be passed saturation values to evaluate the
+underlying function:
+
+```jldoctest
+s = range(0, 1, 50)
+k = s.^2
+kr = PhaseRelativePermeability(s, k)
+round(kr(0.5), digits = 2)
+
+# output
+
+0.25
+```
 """
 function PhaseRelativePermeability(s, k; label = :w, connate = s[1], epsilon = 1e-16)
     s = collect(s)
