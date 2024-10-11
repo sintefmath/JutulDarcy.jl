08ae346eb74a9bfe8da1b4c0525fab75a94846dc
Olav Møyner <olav.moyner@gmail.com>
2024-10-11 13:47:54 +0200

    Update example

--- examples/coarsen_example.jl
+++ examples/coarsen_example.jl
@@ -18,7 +18,7 @@ coarse_mesh = physical_representation(coarse_reservoir)
 
 p = coarse_mesh.partition
 
-plot_cell_data(fine_mesh, p, colormap = :devonS)
+plot_cell_data(fine_mesh, p, colormap = :lipariS)
 ##
 ws_c, states_c = simulate_reservoir(coarse_case)
 ##
@@ -44,16 +44,16 @@ plot_cell_data!(axc, coarse_mesh, p_c, colorrange = caxis)
 for (k, w) in wells
     plot_well!(axc, fine_mesh, w, fontsize = 0)
 end
+##
 
-axf_p = Axis(fig[2, 1], ylabel = "Avg. pressure / bar")
-lines!(axf_p, map(x -> mean(x[:Pressure])/1e5, states))
-
-axf_p = Axis(fig[2, 2], ylabel = "Avg. pressure / bar")
-lines!(axf_p, map(x -> mean(x[:Pressure])/1e5, states_c))
-
+fig = Figure()
+axf_p = Axis(fig[1, 1], ylabel = "Avg. pressure / bar")
+lines!(axf_p, map(x -> mean(x[:Pressure])/1e5, states), label = "Fine")
+lines!(axf_p, map(x -> mean(x[:Pressure])/1e5, states_c), label = "Coarse")
+axislegend()
 fig
 ##
-plot_well_results([ws, ws_c], names = ["Fine", "Coarse"])
+plot_well_results([ws, ws_c], names = ["Fine", "Coarse"], field = :orat, accumulated = true)
 ##
 fine_m = reservoir_measurables(fine_case, ws, states)
 coarse_m = reservoir_measurables(coarse_case, ws_c, states_c)
@@ -65,3 +65,47 @@ for (k, v) in pairs(coarse_m)
 end
 ##
 plot_reservoir_measurables(m, left = :fopr, right = :coarse_fopr, accumulated = true)
+##
+
+partition_variants = [
+    (:ijk, (5, 5, 1)),
+    (:centroids, (3, 3, 2)),
+    (:metis, 10),
+    (:metis, 50)
+]
+
+fig = Figure(size = (1200, 600))
+layout = GridLayout()
+fig[1, 1] = layout
+rowwidth = Int(floor(length(partition_variants)/2))
+for (no, variant) in enumerate(partition_variants)
+    if no > rowwidth
+        row = 2
+        pix = no - rowwidth
+    else
+        row = 1
+        pix = no
+    end
+    cmethod, cdim = variant
+    variant_case = coarsen_reservoir_case(fine_case, cdim, method = cmethod)
+    r = reservoir_domain(variant_case)
+    m = physical_representation(r)
+    p = m.partition
+
+    ax = Axis3(fig, title = "$cmethod - $cdim", azimuth = 0.3, elevation = 1.0, zreversed = true)
+    plot_cell_data!(ax, fine_mesh, p, colormap = :lipariS)
+    layout[row, 2*(pix-1) + 1] = ax
+
+    _, variant_states = simulate_reservoir(variant_case)
+    pres = variant_states[end][:Pressure]
+    axp = Axis3(fig, title = "Pressure", azimuth = 0.3, elevation = 1.0, zreversed = true)
+    for (k, w) in wells
+        plot_well!(axp, fine_mesh, w, fontsize = 0)
+    end
+    plot_cell_data!(axp, m, pres, colorrange = caxis)
+
+    layout[row, 2*(pix-1) + 2] = axp
+end
+fig
+##
+##
