673773dcc5101f77e167ba95c2b11f8401e8b3a1
Olav Møyner <olav.moyner@gmail.com>
2024-10-23 20:22:31 +0200

    Add a basic reservoir version of CUDA linear solver

--- test/gpu.jl
+++ test/gpu.jl
@@ -1,69 +1,60 @@
-using Jutul, JutulDarcy
-using LinearAlgebra, Krylov
-using Printf
-using ForwardDiff
-using CUDA, Test
+using Jutul, JutulDarcy, CUDA, Test
 
-function test_single_phase_gpu(casename = "pico"; float_type = Float32, pvfrac=0.05, tstep = [1.0])#[1.0, 2.0])
-    G, mrst_data = get_minimal_tpfa_grid_from_mrst(casename, extraout = true)
-    nc = number_of_cells(G)
-    # Parameters
+function solve_bl_lsolve(; nx = 10, ny = 1, nstep = nx*ny, lsolve = missing, backend = :csr, step_limit = nothing, kwarg...)
+    time = 1.0
+    T = time
+    tstep = repeat([T/nstep], nstep)
+    mesh = CartesianMesh((nx, ny))
+    domain = reservoir_domain(mesh)
+    nc = number_of_cells(domain)
+    timesteps = tstep*3600*24
     bar = 1e5
-    p0 = 100*bar # 100 bar
-
-    # Single-phase liquid system (compressible pressure equation)
-    phase = LiquidPhase()
-    sys = SinglePhaseSystem(phase)
-    # Simulation model wraps grid and system together with context (which will be used for GPU etc)
-    ctx = SingleCUDAContext(float_type)
-    linsolve = GenericKrylov(Krylov.dqgmres, preconditioner = ILUZeroPreconditioner(), relative_tolerance = 0.01)
-    model = SimulationModel(G, sys, context = ctx)
-
-
-    s = model.secondary_variables
-
-    mu_c = transfer(ctx, repeat([1e-3], 1, nc))
-    mu = ConstantVariables(mu_c, Cells(), single_entity = false)
-    s[:PhaseViscosities] = mu
-
-    s_d = transfer(ctx, ones(1, nc))
-    sat = ConstantVariables(s_d, Cells(), single_entity = false)
-    s[:Saturations] = sat
-    s[:RelativePermeabilities] = sat
-    # System state
-    pv = physical_representation(model).pore_volumes
-    timesteps = tstep*3600*24 # 1 day, 2 days
-    tot_time = sum(timesteps)  
-    irate = pvfrac*sum(pv)/tot_time
-    src = [SourceTerm(1, irate), 
-           SourceTerm(nc, -irate)]
-    src = CuArray(src)
-    forces = setup_forces(model, sources = src)
-
-    # State is dict with pressure in each cell
-    init = Dict(:Pressure => p0)
-    state0 = setup_state(model, init)
-    # Model parameters
+    p0 = 100*bar
+    sys = ImmiscibleSystem((LiquidPhase(), VaporPhase()), reference_densities = [100.0, 100.0])
+    model = setup_reservoir_model(domain, sys, extra_out = false, backend = backend)
+    kr = BrooksCoreyRelativePermeabilities(sys, [2.0, 2.0], [0.2, 0.2])
+    rmodel = reservoir_model(model)
+    c = [1e-3, 1e-3]/bar
+    density = ConstantCompressibilityDensities(
+        p_ref = 100*bar,
+        density_ref = [100.0, 100.0],
+        compressibility = c
+    )
+    replace_variables!(rmodel, RelativePermeabilities = kr, PhaseMassDensities = density)
+    tot_time = sum(timesteps)
+    pv = pore_volume(domain)
+    irate = 500*sum(pv)/tot_time
+    src  = SourceTerm(nc, irate, fractional_flow = [0.75, 0.25])
+    bc = FlowBoundaryCondition(1, p0/2)
+    forces = setup_reservoir_forces(model, sources = src, bc = bc)
     parameters = setup_parameters(model)
-    parameters[:reference_densities] = CuArray(float_type.([1.0]))
-
-    # linsolve = nothing
-    sim = Simulator(model, state0 = state0, parameters = parameters)
-    info_level = 2
-    debug_level = 2
-    simulate(sim, timesteps, forces = forces, linear_solver = linsolve, 
-            debug_level = debug_level, info_level = info_level, output_states = false)
-    return true
+    state0 = setup_reservoir_state(model, Pressure = p0, Saturations = [0.25, 0.75])
+    if ismissing(lsolve)
+        lsolve = reservoir_linsolve(model)
+    end
+    if !isnothing(step_limit)
+        timesteps = timesteps[1:step_limit]
+    end
+    states, report = simulate(state0, model, timesteps; failure_cuts_timestep = false,
+        forces = forces, parameters = parameters, linear_solver = lsolve, kwarg...)
+    return states[end][:Reservoir][:Saturations][1, :]
 end
-if has_cuda_gpu()
-    CUDA.allowscalar(false)
-    casename = "pico"
-    @testset "GPU multiphase" begin
-        @testset "Basic flow - single precision" begin
-            @test test_single_phase_gpu(casename, float_type = Float32)
-        end
-        @testset "Basic flow - double precision" begin
-            @test test_single_phase_gpu(casename, float_type = Float64)
+
+if CUDA.functional()
+    do_solve(x) = solve_bl_lsolve(
+        lsolve = x,
+        info_level = -1
+    )
+    @testset "SimulationModel" begin
+        krylov_cpu = GenericKrylov(:bicgstab, preconditioner = ILUZeroPreconditioner())
+        s_cpu = do_solve(krylov_cpu)
+
+        for T in [Float32, Float64]
+            for solver in [:bicgstab, :gmres]
+                krylov_cu = JutulDarcy.CUDAReservoirKrylov(solver, Float_t = T)
+                s_cu = do_solve(krylov_cu)
+                @test s_cpu ≈ s_cu
+            end
         end
     end
 end
