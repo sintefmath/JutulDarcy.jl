d0cffe61fb07a96f8e8c415570479ea5c5098f5e
Olav Møyner <olav.moyner@gmail.com>
2024-10-25 21:06:47 +0200

    Update format for AMGX config

--- src/ext/ext_amgx.jl
+++ src/ext/ext_amgx.jl
@@ -1,16 +1,16 @@
 struct AMGXPreconditioner <: Jutul.JutulPreconditioner
-    settings::Dict{Symbol, Any}
+    settings::Dict{String, Any}
     data::Dict{Symbol, Any}
-    function AMGXPreconditioner(settings::Dict{Symbol, Any})
+    function AMGXPreconditioner(settings::Dict{String, Any})
         data = Dict{Symbol, Any}()
         new(settings, data)
     end
 end
 
 function AMGXPreconditioner(; kwarg...)
-    settings = Dict{Symbol, Any}()
+    settings = Dict{String, Any}()
     for (k, v) in kwarg
-        settings[k] = v
+        settings["$k"] = v
     end
     return AMGXPreconditioner(settings)
 end
