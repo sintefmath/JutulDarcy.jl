8a55c3fb837f2916b868a3a344750b729fa10db3
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2025-02-11 23:13:23 +0100

    Fix example title formatting

--- examples/geothermal/geothermal_doublet.jl
+++ examples/geothermal/geothermal_doublet.jl
@@ -1,4 +1,4 @@
-# ## Geothermal doublet
+# # Geothermal doublet
 # This example demonstrates how to set up a geothermal doublet simulation using
 # JutulDarcy. We will use two different PVT functions--one simple and one
 # realistic--to highlight the importance of accurate fluid physics in geothermal
