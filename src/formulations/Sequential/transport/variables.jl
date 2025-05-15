037bc44b74467ae5a224ee597e05c93171eb7019
Olav Møyner <olav.moyner@gmail.com>
2025-05-15 09:55:41 +0200

    Make total saturation configurable

--- src/formulations/Sequential/transport/variables.jl
+++ src/formulations/Sequential/transport/variables.jl
@@ -1,5 +1,30 @@
-struct TotalSaturation <: Jutul.ScalarVariable end
-Jutul.minimum_value(::TotalSaturation) = 1e-10
+struct TotalSaturation <: Jutul.ScalarVariable
+    max_abs::Union{Float64, Nothing}
+    max_rel::Union{Float64, Nothing}
+    minimum_value::Float64
+    maximum_value::Float64
+end
+
+function TotalSaturation(;
+        max_abs = nothing,
+        max_rel = 0.2,
+        minimum_value = 1e-3,
+        maximum_value = Inf
+    )
+    if !isnothing(max_abs)
+        max_abs > 0.0 || throw(ArgumentError("Maximum absolute total saturation change was $max_abs, must be positive"))
+    end
+    if !isnothing(max_rel)
+        max_rel > 0.0 || throw(ArgumentError("Maximum relative total saturation change was $max_rel, must be positive"))
+    end
+    minimum_value < maximum_value || throw(ArgumentError("Maximum pressure $maximum_value must be larger than minimum pressure $minimum_pressure"))
+    TotalSaturation(max_abs, max_rel, minimum_value, maximum_value)
+end
+
+Jutul.minimum_value(st::TotalSaturation) = st.minimum_value
+Jutul.maximum_value(st::TotalSaturation) = st.maximum_value
+Jutul.absolute_increment_limit(st::TotalSaturation) = st.max_abs
+Jutul.relative_increment_limit(st::TotalSaturation) = st.max_rel
 
 struct TotalVolumetricFlux <: Jutul.ScalarVariable end
 Jutul.associated_entity(::TotalVolumetricFlux) = Faces()
