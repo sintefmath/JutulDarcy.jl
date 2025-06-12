6b65251b343ea5f940052bca01bfa8ac7440b9a2
Olav Møyner <olav.moyner@gmail.com>
2025-06-12 12:53:38 +0200

    More drafting of opt interface

--- src/gradients/reservoir.jl
+++ src/gradients/reservoir.jl
@@ -1,5 +1,5 @@
-function setup_reservoir_dict_optimization(F, dict)
-    return (DictParameters(dict), F)
+function setup_reservoir_dict_optimization(dict, F = missing)
+    return DictParameters(dict, F)
 end
 
 function setup_reservoir_dict_optimization(case::JutulCase;
@@ -11,14 +11,13 @@ function setup_reservoir_dict_optimization(case::JutulCase;
     is_multimodel = case.model isa MultiModel
     if is_multimodel
         rparameters = case.parameters[:Reservoir]
+        state0_r = case.state0[:Reservoir]
     else
         rparameters = case.parameters
+        state0_r = case.state0
     end
     rmodel = reservoir_model(case.model)
     rdomain = reservoir_domain(rmodel)
-    # TODO: Rename...
-    # data_domain (non-geometry)
-    # wells - WI
 
     skip_list = [
         :satnum,
@@ -45,10 +44,19 @@ function setup_reservoir_dict_optimization(case::JutulCase;
         push!(skip_list, :fluid_thermal_conductivity)
         push!(skip_list, :rock_thermal_conductivity)
     end
+    skip_list_parameters = Symbol[:Transmissibilities]
     DT = OrderedDict{Symbol, Any}
     opt_dict = DT()
+    prm_dict = DT()
+    dd_dict = DT()
+    state0_dict = DT()
+    w_dict = DT()
+    opt_dict[:model] = dd_dict
+    opt_dict[:parameters] = prm_dict
+    opt_dict[:state0] = state0_dict
+    opt_dict[:wells] = w_dict
     if use_trans
-        opt_dict[:transmissibilities] = copy(rparameters[:Transmissibilities])
+        opt_dict[:Transmissibilities] = copy(rparameters[:Transmissibilities])
         push!(skip_list, :permeability)
     end
     if use_pore_volume
@@ -59,7 +67,7 @@ function setup_reservoir_dict_optimization(case::JutulCase;
     for k in setdiff(keys(rdomain), skip_list)
         v = rdomain[k]
         if eltype(v)<:AbstractFloat
-            opt_dict[k] = v
+            dd_dict[k] = v
         end
     end
     if is_multimodel
@@ -71,20 +79,36 @@ function setup_reservoir_dict_optimization(case::JutulCase;
                 if haskey(subprm, :WellIndicesThermal) && is_thermal
                     subdict[:WellIndicesThermal] = subprm[:WellIndicesThermal]
                 end
-                opt_dict[k] = subdict
+                w_dict[k] = subdict
             end
         end
     end
-    dp = DictParameters(opt_dict, strict = strict)
+    # Initial conditions...
+    # TODO: Write F
     F = missing
-    return (dp, F)
+    return DictParameters(opt_dict, F, strict = strict)
 end
 
-function optimize_reservoir(dopt, objective, setup_fn;
-        simulator_arg = NamedTuple(),
+function optimize_reservoir(dopt, objective, setup_fn = dopt.setup_function;
+        simulator_arg = (output_substates = false, ),
         kwarg...
     )
+    if ismissing(setup_fn)
+        error("Setup function was not found in DictParameters struct or as last positional argument.")
+    end
     case0 = setup_fn(dopt.parameters, missing)
-    sim, cfg = setup_reservoir_simulator(case0; simulator_arg...)
-    return Jutul.optimize(dopt, objective, setup_fn; kwarg...)
+    sim, cfg = setup_reservoir_simulator(case0; info_level = -1, simulator_arg...)
+    return Jutul.optimize(dopt, objective, setup_fn; simulator = sim, config = cfg, kwarg...)
+end
+
+function parameters_gradient_reservoir(dopt, objective, setup_fn = dopt.setup_function;
+        simulator_arg = (output_substates = false, ),
+        kwarg...
+    )
+    if ismissing(setup_fn)
+        error("Setup function was not found in DictParameters struct or as last positional argument.")
+    end
+    case0 = setup_fn(dopt.parameters, missing)
+    sim, cfg = setup_reservoir_simulator(case0; info_level = -1, simulator_arg...)
+    return Jutul.parameters_gradient(dopt, objective, setup_fn; simulator = sim, config = cfg, kwarg...)
 end
