a496765b5630fcc13c775efd07c2ab5fc7a4a68d
Olav Møyner <olav.moyner@gmail.com>
2025-06-25 13:31:20 +0200

    Update overloads.jl

--- src/formulations/Sequential/pressure/multimodel/overloads.jl
+++ src/formulations/Sequential/pressure/multimodel/overloads.jl
@@ -13,7 +13,7 @@ function Jutul.update_cross_term_in_entity!(out, i,
     model_s::SimpleWellModel
     sys = model_t.system
     T = eltype(out)
-    out[1] = pressure_perforation_flux(T, ct.parent, i, state_t, state_t, state_s, sys)
+    out[1] = pressure_perforation_flux(T, ct.parent, i, state_t, state_t, state_s, sys, false)
 end
 
 struct PressureWellFromReservoirFlowCT{T} <: Jutul.AdditiveCrossTerm
@@ -30,19 +30,25 @@ function Jutul.update_cross_term_in_entity!(out, i,
     # Target is well, source is reservoir
     model_t::SimpleWellModel
     sys = model_t.system
-    out[1] = -pressure_perforation_flux(eltype(out), ct.parent, i, state_t, state_s, state_t, sys)
+    out[1] = -pressure_perforation_flux(eltype(out), ct.parent, i, state_t, state_s, state_t, sys, true)
 end
 
-function pressure_perforation_flux(T, ct, i, state_dest, state_res, state_well, sys)
+function pressure_perforation_flux(T, ct, i, state_dest, state_res, state_well, sys, is_well)
     conn = cross_term_perforation_get_conn(ct, i, state_well, state_res)
     rhoS = reference_densities(sys)
     ncomp = number_of_components(sys)
     out_full = zeros(T, ncomp)
     q = multisegment_well_perforation_flux!(out_full, sys, state_res, state_well, rhoS, conn)
     val = zero(T)
-    @assert length(q) == size(state_dest.PressureReductionFactors, 1)
+    w = state_dest.PressureReductionFactors
+    @assert length(q) == size(w, 1)
+    if is_well
+        ix = conn.well
+    else
+        ix = conn.reservoir
+    end
     for i in eachindex(q)
-        val += q[i]*state_dest.PressureReductionFactors[i, conn.well]
+        val += q[i]*w[i, ix]
     end
     return val
 end
