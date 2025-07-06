0207c469c206f84dd75b7c904b3163bbed9eb6f0
Olav Møyner <olav.moyner@gmail.com>
2025-07-06 19:53:45 +0200

    Fix objective format in example (#151)

--- examples/validation/validation_compositional.jl
+++ examples/validation/validation_compositional.jl
@@ -92,8 +92,8 @@ end
 # report step that was plotted in the previous section.
 import Statistics: mean
 import JutulDarcy: reservoir_sensitivities
-function objective_function(model, state, Δt, step_i, forces)
-    if step_i != step_to_plot
+function objective_function(model, state, Δt, step_info, forces)
+    if step_info[:step] != step_to_plot
         return 0.0
     end
     sg = @view state.Reservoir.Saturations[2, :]
