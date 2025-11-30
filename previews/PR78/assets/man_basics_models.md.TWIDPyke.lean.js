a4a827f75009a98fed49c65f38dbe4c2edb21f74
Documenter.jl <documenter@juliadocs.github.io>
2025-11-30 13:07:45 +0000

    delete history

---
+++ previews/PR78/assets/man_basics_models.md.TWIDPyke.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as s}from"./chunks/framework.C7HFQ_i9.js";const p=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"man/basics/models.md","filePath":"man/basics/models.md","lastUpdated":null}'),t={name:"man/basics/models.md"};function o(n,c,r,d,m,l){return a(),s("div")}const _=e(t,[["render",o]]);export{p as __pageData,_ as default};
