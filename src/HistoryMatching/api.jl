6b8219f2cb90134f6a1a4b34e61ab523b9a0a4d9
Olav Møyner <olav.moyner@gmail.com>
2025-12-04 15:43:26 +0100

    Add some more scalers

--- src/HistoryMatching/api.jl
+++ src/HistoryMatching/api.jl
@@ -35,14 +35,30 @@ function match_well!(hm::HistoryMatch, name::Union{String, Symbol}, quantity::Un
         is_injector::Bool,
         data = missing,
         t = missing,
+        scale = missing,
         is_sum_objective::Bool = false
     )
-    scale = 1.0
     is_cumulative = false
     rmodel = reservoir_model(hm.case)
     sys = rmodel.system
-    rhos = JutulDarcy.reference_densities(sys)
+    rhows = phase_reference_density(sys, JutulDarcy.AqueousPhase())
+    rhoos = phase_reference_density(sys, JutulDarcy.LiquidPhase())
+    rhogs = phase_reference_density(sys, JutulDarcy.VaporPhase())
+    rhols = 0.5*(rhoos + rhows)
+
     day = si_unit(:day)
+    # Rates
+    grat_scale = day/rhogs
+    orat_scale = day/rhoos
+    wrat_scale = day/rhows
+    lrat_scale = day/rhols
+    bhp_scale = 1.0/(100*si_unit(:bar))
+    # Cumulative production
+    gtotal_scale = 1.0./rhogs
+    ototal_scale = 1.0./rhoos
+    wtotal_scale = 1.0./rhows
+    ltotal_scale = 1.0./rhols
+
     if is_injector
         if quantity isa Symbol
             if quantity == :rate
@@ -62,14 +78,19 @@ function match_well!(hm::HistoryMatch, name::Union{String, Symbol}, quantity::Un
         quantity = uppercase(quantity)
         if quantity == "RATE"
             dest = hm.injector_rate
+            dscale = lrat_scale
         elseif quantity == "WGIR"
             dest = hm.injector_grat
+            dscale = grat_scale
         elseif quantity == "WOIR"
             dest = hm.injector_orat
+            dscale = orat_scale
         elseif quantity == "WWIR"
             dest = hm.injector_wrat
+            dscale = wrat_scale
         elseif quantity == "WBHP"
             dest = hm.injector_bhp
+            dscale = bhp_scale
         else
             error("Unsupported quantity '$quantity' for injector well match.")
         end
@@ -102,34 +123,38 @@ function match_well!(hm::HistoryMatch, name::Union{String, Symbol}, quantity::Un
         quantity = uppercase(quantity)
         if quantity == "RATE"
             dest = hm.producer_rate
-            dscale = day
+            dscale = lrat_scale
         elseif quantity == "WBHP"
             dest = hm.producer_bhp
-            dscale = 1.0/si_unit(:bar)
+            dscale = bhp_scale
         elseif quantity == "WGPR"
             dest = hm.producer_grat
+            dscale = grat_scale
         elseif quantity == "WOPR"
             dest = hm.producer_orat
+            dscale = orat_scale
         elseif quantity == "WLPR"
             dest = hm.producer_lrat
-            # Don't know what phases are in the model, so just assume 1000
-            dscale = day/1000.0
+            dscale = lrat_scale
         elseif quantity == "WWPR"
             dest = hm.producer_wrat
-            idx = JutulDarcy.phase_index(sys, :water)
-            dscale = day/rhos[idx]
+            dscale = wrat_scale
         elseif quantity == "WOPT"
             dest = hm.producer_cumulative_oil
             is_cumulative = true
+            dscale = gtotal_scale
         elseif quantity == "WGPT"
             dest = hm.producer_cumulative_gas
             is_cumulative = true
+            dscale = ototal_scale
         elseif quantity == "WWPT"
             dest = hm.producer_cumulative_water
             is_cumulative = true
+            dscale = wtotal_scale
         elseif quantity == "WLPT"
             dest = hm.producer_cumulative_liquid
             is_cumulative = true
+            dscale = ltotal_scale
         else
             error("Unsupported quantity '$quantity' for producer well match.")
         end
@@ -141,10 +166,24 @@ function match_well!(hm::HistoryMatch, name::Union{String, Symbol}, quantity::Un
     if weight isa Vector
         length(weight) == length(case.dt) || error("Weight vector length must match number of simulation report steps in case ($(length(case.dt))).")
     end
+    if ismissing(scale)
+        scale = dscale
+    end
     wm = WellMatch(Symbol(name), quantity, weight, scale, is_injector, data)
     # total rates can be computed from other rates...
     push!(dest, wm)
     return hm
+end
+
+function phase_reference_density(sys, phase)
+    if phase in JutulDarcy.get_phases(sys)
+        idx = JutulDarcy.phase_index(sys, VaporPhase())
+        rhos = JutulDarcy.reference_densities(sys)
+        val = rhos[idx]
+    else
+        val = 1000.0
+    end
+    return val
 end
 
 function match_injectors!(hm::HistoryMatch, quantity::Union{String, Symbol}, wells = get_injectors(hm); kwarg...)
