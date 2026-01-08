4b64e71fed8eaf9dfb77f90049d278a6a9958097
Olav Møyner <olav.moyner@gmail.com>
2026-01-08 14:39:16 +0100

    Update afi_utils.jl

--- src/AFISetup/afi_utils.jl
+++ src/AFISetup/afi_utils.jl
@@ -29,3 +29,78 @@ function merge_records(a, b)
     end
     return out
 end
+
+function obsh_to_summary(obsh, t_seconds = missing; start_date = missing)
+    if ismissing(t_seconds)
+        WI = obsh["wells_interp"]
+        w_sample = first(values(WI))
+        t_seconds = w_sample["seconds"]
+    end
+    summary_obs = Dict()
+    summary_obs["TIME"] = (start_date = start_date, seconds = copy(t_seconds))
+    summary_obs["VALUES"] = Dict()
+    summary_obs["VALUES"]["WELLS"] = Dict()
+
+    function get_data_obsh(name, t, cumulative = false)
+        wdata = obsh["wells_interp"][name]
+        t_i = t_jutul.*si_unit(:day)
+        if t == :wwir
+            if cumulative
+                ix = to_cumulative(t_i, wdata["WATER_INJECTION_RATE"].(t_i))
+            else
+                ix = wdata["WATER_INJECTION_RATE"].(t_i).*si_unit(:day)
+            end
+        elseif t == :wwpr
+            if cumulative
+                ix = to_cumulative(t_i, wdata["WATER_PRODUCTION_RATE"].(t_i))
+            else
+                ix = wdata["WATER_PRODUCTION_RATE"].(t_i).*si_unit(:day)
+            end
+        elseif t == :wopr
+            if cumulative
+                ix = to_cumulative(t_i, wdata["OIL_PRODUCTION_RATE"].(t_i))
+            else
+                ix = wdata["OIL_PRODUCTION_RATE"].(t_i).*si_unit(:day)
+            end
+        elseif t == :lrat
+            if cumulative
+                ix = to_cumulative(t_i, wdata["LIQUID_PRODUCTION_RATE"].(t_i))
+            else
+                ix = wdata["LIQUID_PRODUCTION_RATE"].(t_i).*si_unit(:day)
+            end
+        elseif t == :wgpr
+            return 0 .* t_i
+            if cumulative
+                ix = to_cumulative(t_i, wdata["GAS_PRODUCTION_RATE"].(t_i))
+            else
+                ix = wdata["GAS_PRODUCTION_RATE"].(t_i).*si_unit(:day)
+            end
+        elseif t == :wbhp
+            if cumulative
+                error("Cumulative BHP makes no sense")
+            else
+                ix = wdata["BOTTOM_HOLE_PRESSURE"].(t_i)./si_unit(:bar)
+            end
+        end
+        return ix
+    end
+
+    t_s = summary_obs["TIME"].seconds
+    dt_s = diff([0; t_s])
+    for well in keys(obsh["wells_interp"])
+        swdata = Dict()
+        wopt = obsh["wells_interp"][well]["OIL_PRODUCTION_CUML"].(t_s)
+        swdata["WOPT"] = wopt
+        swdata["WOPR"] = diff([0; wopt])./dt_s
+        wwpt = obsh["wells_interp"][well]["WATER_PRODUCTION_CUML"].(t_s)
+        swdata["WWPT"] = wwpt
+        swdata["WWPR"] = diff([0; wwpt])./dt_s
+        wwit = obsh["wells_interp"][well]["WATER_INJECTION_CUML"].(t_s)
+        swdata["WWIT"] = wwit
+        swdata["WWIR"] = diff([0; wwit])./dt_s
+        swdata["WBHP"] = obsh["wells_interp"][well]["BOTTOM_HOLE_PRESSURE"].(t_s)
+        summary_obs["VALUES"]["WELLS"][string(well)] = swdata
+    end
+    summary_obs["UNIT_SYSTEM"] = "si"
+    return summary_obs
+end
