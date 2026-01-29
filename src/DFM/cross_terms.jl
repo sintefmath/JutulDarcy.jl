4a317b88425b879a1999a8eb6746b7cd50e70c60
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-01-30 00:12:52 +0100

    Add fracture-well cross-term sketch

--- src/DFM/cross_terms.jl
+++ src/DFM/cross_terms.jl
@@ -77,3 +77,58 @@ function Jutul.subcrossterm(ct::MatrixFromFractureFlowCT, ctp, m_t, m_s, map_res
 
     return MatrixFromFractureFlowCT(mc_local, fc_local, T)
 end
+
+struct FracturesFromWellFlowCT{I<:AbstractVector} <: AbstractReservoirFromWellCT
+    fracture_cells::I
+    well_cells::I
+end
+
+function Jutul.cross_term_entities(ct::FracturesFromWellFlowCT, eq::ConservationLaw, model)
+    return ct.fracture_cells
+end
+
+function Jutul.cross_term_entities_source(ct::FracturesFromWellFlowCT, eq::ConservationLaw, model)
+    return ct.well_cells
+end
+
+# Jutul.can_impact_cross_term(force_t::PerforationMask, cross_term::AbstractReservoirFromWellCT) = true
+
+function update_cross_term_in_entity!(out, i,
+    state_t, state0_t,
+    state_s, state0_s, 
+    model_t, model_s,
+    ct::FracturesFromWellFlowCT, eq, dt, ldisc = local_discretization(ct, i))
+    sys = model_t.system
+    rhoS = reference_densities(sys)
+    conn = cross_term_perforation_get_fracture_conn(ct, i, state_s, state_t)
+    # Call smaller interface that is easy to specialize
+    if haskey(state_s, :MassFractions)
+        @inbounds simple_well_perforation_flux!(out, sys, state_t, state_s, rhoS, conn)
+    else
+        @inbounds multisegment_well_perforation_flux!(out, sys, state_t, state_s, rhoS, conn)
+    end
+    return out
+end
+
+function cross_term_perforation_get_fracture_conn(ct, i, state_s, state_t)
+    @inbounds begin 
+        fracture_cell = ct.fracture_cells[i]
+        well_cell = ct.well_cells[i]
+        WI = state_s.FractureWellIndices[i]
+        gdz = state_s.PerforationGravityDifference[i]
+        p_well = state_s.Pressure
+        p_res = state_t.Pressure
+        dp = p_well[well_cell] - p_res[fracture_cell]
+    end
+
+    # Wrap the key connection data in tuple for easy extension later
+    conn = (
+        dp = dp,
+        WI = WI,
+        gdz = gdz,
+        well = well_cell,
+        perforation = i,
+        reservoir = fracture_cell
+    )
+    return conn
+end
