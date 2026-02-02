a826146e8f9771feb53c10666fba9af12436f901
Olav Møyner <olav.moyner@gmail.com>
2026-02-02 20:02:24 +0100

    Improvements to smoother for OBSH

--- src/AFISetup/afi_utils.jl
+++ src/AFISetup/afi_utils.jl
@@ -30,7 +30,12 @@ function merge_records(a, b)
     return out
 end
 
-function obsh_to_summary(obsh, t_seconds = missing; start_date = missing, smooth = false, alpha = 0.7, remove_bhp_missing = true)
+function obsh_to_summary(obsh, t_seconds = missing;
+        start_date = missing,
+        smooth = false,
+        alpha = 0.7,
+        remove_bhp_missing = true
+    )
     if ismissing(t_seconds)
         WI = obsh["wells_interp"]
         w_sample = first(values(WI))
@@ -65,7 +70,7 @@ function obsh_to_summary(obsh, t_seconds = missing; start_date = missing, smooth
                 I_bhp = remove_missing(I_bhp)
             end
             if smooth
-                I_bhp = smooth_interpolant(I_bhp, alpha)
+                I_bhp = smooth_interpolant(I_bhp, alpha, sentinel = 0.0)
             end
             swdata["WBHP"] = I_bhp.(t_s)
         end
@@ -123,10 +128,6 @@ function get_obsh_rate_and_cumulative(well, prefix, obsh, t_s; smooth = false, a
     else
         if smooth
             rate_interp = smooth_interpolant(rate_interp, alpha)
-            # rate_val = rate_interp.F
-            # rate_t = rate_interp.X
-            # rate_vals_smooth = exponential_smoothing(rate_val, alpha)
-            # rate_interp = get_linear_interpolant(rate_t, rate_vals_smooth)
         end
         rate = rate_interp.(t_s)
 
@@ -166,17 +167,23 @@ function remove_missing(I::Jutul.LinearInterpolant)
     end
 end
 
-function smooth_interpolant(I::Jutul.LinearInterpolant, alpha::Float64)
+function smooth_interpolant(I::Jutul.LinearInterpolant, alpha::Float64; sentinel = missing)
     vals = I.F
-    smoothed_vals = exponential_smoothing(vals, alpha)
+    smoothed_vals = exponential_smoothing(vals, alpha, sentinel)
     return Jutul.get_1d_interpolator(I.X, smoothed_vals, cap_endpoints = true)
 end
 
-function exponential_smoothing(data::Vector{Float64}, alpha::Float64)
+function exponential_smoothing(data::Vector{Float64}, alpha::Float64, sentinel = missing)
     alpha < 0.0 && error("Alpha must be in [0, 1], got $alpha")
     alpha > 1.0 && error("Alpha must be in [0, 1], got $alpha")
     smoothed = copy(data)
     for i in 2:length(data)
+        if !ismissing(sentinel)
+            if isapprox(data[i], sentinel; atol = 1e-8)
+                smoothed[i] = data[i]
+                continue
+            end
+        end
         smoothed[i] = alpha * data[i] + (1 - alpha) * smoothed[i - 1]
     end
     return smoothed
