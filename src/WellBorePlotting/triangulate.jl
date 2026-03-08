0805e001c7f6cdd119bd2abf955e48ed641f11f4
Olav Møyner <olav.moyner@gmail.com>
2026-03-08 21:20:18 +0100

    Update triangulate.jl

--- src/WellBorePlotting/triangulate.jl
+++ src/WellBorePlotting/triangulate.jl
@@ -12,11 +12,24 @@ function triangulate_well_domain(w::DataDomain; rfactor = 10.0, outer = false, i
     else
         pts = w[:cell_centroids]
     end
+    # if size(pts, 2) == 1
+    #     pts = repeat(pts, 1, 2)
+    #     pt_remap = [1, 1]
+    # else
+    #     pt_remap = 1:size(pts, 2)
+    # end
+    pt_remap = collect(1:size(pts, 2))
+    if size(pts, 1) == 3
+        pushfirst!(pt_remap, 1)
+        pts = hcat(pts[:, 1], pts)
+        pts[3, 1] -= 10.0
+    end
+
     radius = w[:radius] .* rfactor
     r = sum(radius) / length(radius)
 
     ctri = triangulate_cylindrical_mesh(pts, radius = r, cap = :round)
-    cell_index = ctri.vert_srcidx
+    cell_index = pt_remap[ctri.vert_srcidx]
     if is_simple
         cell_index .= 1
     end
