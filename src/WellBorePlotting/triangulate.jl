69054409c2d8d84fcca7d396556b45c4cf5f62a0
Olav Møyner <olav.moyner@gmail.com>
2026-03-08 20:52:39 +0100

    Update triangulate.jl

--- src/WellBorePlotting/triangulate.jl
+++ src/WellBorePlotting/triangulate.jl
@@ -1,3 +1,27 @@
-function Jutul.triangulate_mesh(m::Union{MultiSegmentWell, SimpleWell})
-
+function Jutul.triangulate_mesh(m::DataDomain{T}; kwarg...) where {T<:SimpleWell}
+    return triangulate_well_domain(m; is_simple = true, kwarg...)
 end
+
+function Jutul.triangulate_mesh(m::DataDomain{T}; kwarg...) where {T<:MultiSegmentWell}
+    return triangulate_well_domain(m; is_simple = false, kwarg...)
+end
+
+function triangulate_well_domain(w::DataDomain; rfactor = 10.0, outer = false, is_simple = false, flatten = true, flip = false)
+    if is_simple
+        error("Triangulation of SimpleWell is not implemented yet")
+    else
+        pts = w[:cell_centroids]
+        radius = w[:radius] .* rfactor
+
+        r = sum(radius) / length(radius)
+    end
+    ctri = triangulate_cylindrical_mesh(pts, radius = r, cap = :round)
+    cell_index = ctri.vert_cell
+    @info "???" cell_index size(pts)
+    mapper = (
+                Cells = (cell_data) -> cell_data[cell_index],
+                indices = (Cells = cell_index, )
+            )
+    return (points = ctri.xyz, triangulation = ctri.tri, mapper = mapper)
+end
+
