02c69a768f750ef32202ad43b6b509f10512beda
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-03-20 22:34:21 +0100

    Functionality for adding fractures to a model

--- src/DFM/utils.jl
+++ src/DFM/utils.jl
@@ -134,33 +134,76 @@ end
 function add_fractures_to_model!(model::MultiModel, fractures::DataDomain; kwarg...)
     
     # Get matrix model
-    matrix = model.models[:Reservoir]
-    system = matrix.system
+    matrix_model = model.models[:Reservoir]
+    system = matrix_model.system
 
+    # Set up fracture model with same system and context as matrix model
+    fracture_model = setup_reservoir_model(fractures, system;
+        context=model.context, kwarg...)
+    if fracture_model isa Jutul.MultiModel
+        fracture_model = fracture_model.models[:Reservoir]
+    end
+    # Add extra entities to fracture model for fracture-matrix connections
+    fmc = JutulDarcy.FractureMatrixConnection()
+    fracture_model.domain.entities[fmc] = count_entities(fractures, fmc)
+
+    # Block flow across matrix faces that are connected to fractures
     fmc = JutulDarcy.FractureMatrixConnection()
     if haskey(fractures, :matrix_faces)
         matrix_faces = fractures[:matrix_faces, fmc]
-        block_fracture_face_connections!(matrix, unique(matrix_faces))
+        block_fracture_face_connections!(matrix_model, unique(matrix_faces))
+    else
+        @warn "Matrix faces corresponding to fractures not found in fracture domain. \
+        Fracture faces will not be blocked and fractures will not affect flow \
+        through matrix cells unless already accounted for in the matrix domain."
     end
+    if haskey(fractures, :matrix_cells)
+        adjust_matrix_cell_volumes!(matrix_model, fractures)
+    else
+        @warn "Matrix cells"
+    end
+
+    # Add fracture perforations to wells
+    matrix_mesh = physical_representation(matrix_model.domain)
+    wells = []
+    for (name, well) in get_model_wells(model; data_domain=true)
+        well = add_fractures_to_well(well, fractures, matrix_mesh, unique(matrix_faces))
+        push!(wells, well)
+    end
+
+    # Set up new model with fracture model and updated wells TODO: This
+    # reconstructs the entire model, which may miss some of the internal
+    # structure of the original model. We should find a way to ensure the
+    # original model structure is preserved.
+    model = setup_reservoir_model(matrix_model.data_domain, system;
+        wells=wells,
+        thermal=model_is_thermal(matrix_model),
+        context=model.context, kwarg...)
+
+    # Add the fracture model to the multimodel
+    model = add_fracture_model(model, fracture_model)
+
+    # Set up DFM cross-terms
+    add_fracture_cross_terms!(model, fractures)
 
     return model
 
 end
 
-function block_fracture_face_connections!(matrix::SimulationModel, faces)
+function block_fracture_face_connections!(matrix_model::SimulationModel, faces)
 
-    thermal = model_is_thermal(matrix)
-    data_domain = matrix.data_domain
+    thermal = model_is_thermal(matrix_model)
+    matrix = matrix_model.data_domain
 
     # Set zero transmissibility for fracture faces
-    if haskey(data_domain, :transmissibilities)
-        T = data_domain[:transmissibilities, Faces()]
+    if haskey(matrix, :transmissibilities)
+        T = matrix[:transmissibilities, Faces()]
     else
         T = Jutul.default_parameter_values(
-            data_domain, matrix, Transmissibilities(), :Transmissibilities)
+            matrix, matrix_model, Transmissibilities(), :Transmissibilities)
     end
     T[faces] .= 0.0
-    data_domain[:transmissibilities, Faces()] = T
+    matrix[:transmissibilities, Faces()] = T
     
     thermal || return
     
@@ -169,30 +212,255 @@ function block_fracture_face_connections!(matrix::SimulationModel, faces)
         Λr = matrix[:rock_thermal_conductivities, Faces()]
     else
         Λr = Jutul.default_parameter_values(
-            data_domain, matrix, RockThermalConductivities(), :RockThermalConductivities)
+            matrix, matrix_model, RockThermalConductivities(), :RockThermalConductivities)
     end
     Λr[faces] .= 0.0
-    data_domain[:rock_thermal_conductivities, Faces()] = Λr
+    matrix[:rock_thermal_conductivities, Faces()] = Λr
 
     # Set zero fluid thermal conductivity for fracture faces
     if haskey(matrix, :fluid_thermal_conductivities)
         Λf = matrix[:fluid_thermal_conductivities, Faces()]
     else
         Λf = Jutul.default_parameter_values(
-            data_domain, matrix, FluidThermalConductivities(), :FluidThermalConductivities)
+            matrix, matrix_model, FluidThermalConductivities(), :FluidThermalConductivities)
     end
     Λf[faces] .= 0.0
-    data_domain[:fluid_thermal_conductivities, Faces()] = Λf
+    matrix[:fluid_thermal_conductivities, Faces()] = Λf
 
     return
 
 end
 
-function add_fractures_to_well()
+function adjust_matrix_cell_volumes!(matrix_model::SimulationModel, fractures::DataDomain)
 
+    matrix = matrix_model.data_domain
+
+    fracture_volumes = fractures[:volumes, Cells()]
+    matrix_volumes = matrix[:volumes, Cells()]
+    fmc = JutulDarcy.FractureMatrixConnection()
+    fracture_cells = fractures[:connection_cells, fmc]
+    matrix_cells = fractures[:matrix_cells, fmc]
+    for (cf, mc) in zip(fracture_cells, matrix_cells)
+        vf = fracture_volumes[cf]
+        matrix_volumes[mc] -= vf/2
+    end
+    matrix[:volumes, Cells()] = matrix_volumes
+
+    return
 
 end
 
+function add_fractures_to_well(well, fractures::DataDomain, matrix_mesh, matrix_faces; kwarg...)
+
+    well0 = well
+    neighborship, perforation_cells_fractures, perforation_cells_fractures_self, old_ix = 
+    add_fracture_cells(matrix_mesh, well0.representation, matrix_faces)
+
+    has_fracture_perforations = length(perforation_cells_fractures) > 0
+    if !has_fracture_perforations
+        return well0
+    end
+
+    num_segments = size(neighborship, 2)
+    segment_models = [SegmentWellBoreFrictionHB() for _ in 1:num_segments]
+    for seg = 1:num_segments
+        if old_ix[seg] != 0
+            segment_models[seg] = well0.representation.segment_models[old_ix[seg]]
+        end
+    end
+
+    perf = (
+        self = well0.representation.perforations.self,
+        reservoir = well0.representation.perforations.reservoir,
+        self_fracture = perforation_cells_fractures_self,
+        fracture = perforation_cells_fractures,
+    )
+
+    # TODO: Find radii and direction nearest fracture perforations
+    radius = well0[:perforation_radius, Perforations()][1]
+    dir = well0[:perforation_direction, Perforations()][1]
+    K_fractures = fractures[:permeability]
+    # Compute effective thermal conductivity
+    Λf = fractures[:fluid_thermal_conductivity]
+    Λr = fractures[:rock_thermal_conductivity]
+    if haskey(fractures, :net_to_gross)
+        ntg = fractures[:net_to_gross]
+    else
+        ntg = missing
+    end
+    ϕ = fractures[:porosity]
+    Λr = vec(Λr)
+    ϕ = vec(ϕ)
+    if size(Λf, 1) == 1 || Λf isa Vector
+        Λf = vec(Λf)
+        Λ = ϕ.*Λf + (1.0 .- ϕ).*Λr
+    else
+        # TODO: This is a bit of a hack. We should really have a proper way to
+        # do this inside the equations for multiphase flow.
+        Λ = Λr
+    end
+
+    if has_fracture_perforations
+        fracture_mesh = physical_representation(fractures)
+        tmp_frac = setup_well(fracture_mesh, K_fractures, perforation_cells_fractures;
+        simple_well=false, radius=radius, dir=dir, net_to_gross=ntg,
+        thermal_conductivity=Λ,
+        kwarg...)
+    else
+        tmp_frac = missing
+    end
+
+    W = MultiSegmentWell(
+        well0.representation.type,
+        maximum(neighborship, init = 1),
+        length(segment_models),
+        length(well0.representation.perforations.self),
+        perf,
+        neighborship,
+        well0.representation.end_nodes,
+        well0.representation.surface,
+        well0.representation.name,
+        segment_models,
+    )
+
+    well = DataDomain(W)
+    for (k, v) in well0.data
+        val, entity = v
+        if ismissing(tmp_frac)
+            well[k, entity] = val
+            continue
+        end
+        frac_val = tmp_frac[k, entity]
+        if entity == Cells()
+            # Cell data
+            if k ∈ [:cell_centroids, :perforation_centroids]
+                new_val = hcat(val, frac_val)
+            else
+                new_val = vcat(val, frac_val)
+            end
+        elseif entity == Faces()
+            new_val = similar(val, count_entities(W, Faces()))
+            new_val[old_ix .!= 0] = val[old_ix[old_ix .!= 0]]
+            fixed = old_ix .!= 0
+            # Insert nearby values for new fracture perforation faces
+            while any(.!fixed)
+                for seg in 1:num_segments
+                    if !fixed[seg]
+                        pseg = max(seg-1, 1)
+                        nseg = min(seg+1, num_segments)
+                        if fixed[pseg]
+                            new_val[seg] = new_val[pseg]
+                            fixed[seg] = true
+                        elseif fixed[nseg]
+                            new_val[seg] = new_val[nseg]
+                            fixed[seg] = true
+                        end
+                    end
+                end
+            end
+        elseif entity == Perforations()
+            well[k, entity] = val
+            new_val = frac_val
+            k = Symbol(String(k)*"_frac")
+            entity = FracturePerforations()
+        end
+        well[k, entity] = new_val
+    end
+
+    wc = well.representation.perforations.self_fracture
+    fc = well.representation.perforations.fracture
+    
+    well[:cell_length][wc] = fractures[:aperture][fc]
+    Δ = well[:cell_dims_frac, FracturePerforations()]
+    dir = well[:perforation_direction_frac, FracturePerforations()]
+    for (c, (Δ_k, dir_k)) in enumerate(zip(Δ, dir))
+        if dir_k isa Symbol
+            d = findfirst(==(dir_k), [:x, :y, :z])
+        else
+            d = last(findmax(abs.(dir_k)))
+        end
+        Δ_k = [Δ_k...]
+        Δ_k[d] = fractures[:aperture][fc[c]]
+        Δ[c] = Tuple(Δ_k)
+    end
+    well[:cell_dims_frac, FracturePerforations()] = Δ
+
+    return well
+
+end
+
+function add_fracture_model(model::MultiModel, fracture_model::SimulationModel)
+
+    new_models = JutulStorage()
+    new_models[:Reservoir] = model.models[:Reservoir]
+    new_models[:Fractures] = fracture_model
+    groups = Int[1, 1]
+    for (k, v) in pairs(model.models)
+        if k != :Reservoir
+            new_models[k] = v
+            push!(groups, 2)
+        end
+    end
+    if isnothing(model.groups)
+        groups = nothing
+    end
+    model0 = model
+    model = Jutul.MultiModel(new_models; groups=groups)
+    for ct in model0.cross_terms
+        push!(model.cross_terms, ct)
+    end
+
+    return model
+
+end
+
+function add_fracture_cross_terms!(model::MultiModel, fractures::DataDomain)
+    # Set up DFM cross-terms
+
+    fmc = JutulDarcy.FractureMatrixConnection()
+
+    set_parameters!(model.models[:Fractures],
+        FractureMatrixTransmissibility = FractureMatrixTransmissibility(),
+        FractureMatrixGravityDifference = FractureMatrixGravityDifference(),
+    )
+    matrix_cells = fractures[:matrix_cells, fmc]
+    fracture_cells = fractures[:connection_cells, fmc]
+    ct = MatrixFromFractureFlowCT(matrix_cells, fracture_cells)
+    add_cross_term!(model, ct, target = :Reservoir, source = :Fractures, equation = :mass_conservation)
+
+    thermal = model_is_thermal(model.models[:Reservoir])
+    if thermal
+        set_parameters!(model.models[:Fractures],
+            FractureMatrixThermalConductivity = FractureMatrixThermalConductivity(),
+        )
+        ct = MatrixFromFractureThermalCT(matrix_cells, fracture_cells)
+        add_cross_term!(model, ct, target = :Reservoir, source = :Fractures, equation = :energy_conservation)
+    end
+
+    for (name, well_model) in get_model_wells(model)
+        g = physical_representation(well_model)
+        if g isa WellDomain
+            if !haskey(g.perforations, :fracture)
+                continue
+            end
+            set_parameters!(model.models[name],
+                FractureWellIndices = FractureWellIndices()
+            )
+            fc = vec(g.perforations.fracture)
+            wc = vec(g.perforations.self_fracture)
+            ct = FracturesFromWellFlowCT(fc, wc)
+            add_cross_term!(model, ct, target = :Fractures, source = name, equation = :mass_conservation)
+            if thermal
+                ct = FracturesFromWellThermalCT(fc, wc)
+                add_cross_term!(model, ct, target = :Fractures, source = name, equation = :energy_conservation)
+                set_parameters!(model.models[name],
+                    FractureWellIndicesThermal = FractureWellIndicesThermal()
+                )
+            end
+        end
+    end
+
+end
 
 function setup_fractured_reservoir_model(matrix::DataDomain, fractures::DataDomain, system::Union{JutulSystem, Symbol};
     block_backend = true,
