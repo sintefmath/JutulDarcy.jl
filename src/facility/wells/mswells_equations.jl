d527eef079cd2fc8e7de85d4e2efcc00f500621c
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-04-17 15:05:08 +0200

    Use ifelse instead of ternary

--- src/facility/wells/mswells_equations.jl
+++ src/facility/wells/mswells_equations.jl
@@ -107,8 +107,8 @@ function Jutul.update_equation_in_entity!(eq_buf, i, state, state0, eq::Potentia
     else
         rho_l, mu_l = saturation_mixed(s, densities, μ, left)
         rho_r, mu_r = saturation_mixed(s, densities, μ, right)
-        rho = V > 0 ? rho_l : rho_r # Upwind
-        μ_mix = V > 0 ? mu_l : mu_r # Upwind
+        rho = ifelse(V >= 0, rho_l, rho_r) # Upwind
+        μ_mix = ifelse(V >= 0, mu_l, mu_r) # Upwind
         Δp = segment_pressure_drop(seg_model, L, roughness, radius_outer, radius_inner, V, rho, μ_mix)
         Δθ = two_point_potential_drop(p[left], p[right], gdz, rho_l, rho_r)
         eq = Δθ - Δp
