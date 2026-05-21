c18d4d1fa2f33a68a64c5191fa6186694e19c725
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-05-21 12:15:45 +0200

    Add cross-term for surface enthalpy + improve well injection enthalpy logic

--- src/utils.jl
+++ src/utils.jl
@@ -1450,6 +1450,8 @@ function setup_reservoir_cross_terms!(model::MultiModel)
                     add_cross_term!(model, ct, target = target_well, source = k, equation = energy)
                     ct = FacilityFromWellTemperatureCT(target_well)
                     add_cross_term!(model, ct, target = k, source = target_well, equation = :temperature_equation)
+                    ct = FacilityFromWellEnthalpyCT(target_well)
+                    add_cross_term!(model, ct, target = k, source = target_well, equation = :enthalpy_equation)
                 end
             end
         else
