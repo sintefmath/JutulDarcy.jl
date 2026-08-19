607cea0cc381ae52224e7b705ec617384559144a
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-08-19 22:16:50 +0200

    Move generate_perm_poro to extension for now

--- src/ext/ext_geostats.jl
+++ src/ext/ext_geostats.jl
@@ -1,20 +1,11 @@
 """
-    kozeny_carman_permeability(porosity; kwarg...)
-
-Compute permeability from porosity.
-
-Implementation is provided by `JutulDarcyGeoStatsExt` when `GeoStats` is loaded.
-"""
-function kozeny_carman_permeability end
-
-"""
     setup_perm_poro_realizations(dims, box_lengths; kwarg...)
 
 Generate porosity/permeability realizations over a box.
 
 Implementation is provided by `JutulDarcyGeoStatsExt` when `GeoStats` is loaded.
 """
-function setup_perm_poro_realizations end
+function generate_perm_poro end
 
 """
     map_to_domain(domain, property; box, mapping = :mean, copy_domain = false, info_level = 0)
@@ -32,10 +23,3 @@ Mutating variant of `map_to_domain` that stores the mapped values in the named
 property on the domain.
 """
 function map_to_domain! end
-
-"""
-    map_realization_to_reservoir_domain(domain, realization; copy_domain = true)
-
-Backward-compatible wrapper around `map_to_domain` for legacy realization-based inputs.
-"""
-function map_realization_to_reservoir_domain end
