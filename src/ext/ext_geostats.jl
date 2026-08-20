5c9071d113f0ba2fa0f823ec6d271bdfea839e0d
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2026-08-20 09:13:47 +0200

    Cleanup

--- src/ext/ext_geostats.jl
+++ src/ext/ext_geostats.jl
@@ -7,20 +7,20 @@ Implementation is provided by `JutulDarcyGeoStatsExt` when `GeoStats` is loaded.
 """
 function generate_perm_poro end
 
-"""
-    map_to_domain(domain, property; mapping = :mean, info_level = 0, name = missing)
+# """
+#     map_to_domain(domain, property; mapping = :mean, info_level = 0, name = missing)
 
-Map a property sampled on a GeoStats grid or point cloud onto a JutulDarcy
-reservoir domain.
+# Map a property sampled on a GeoStats grid or point cloud onto a JutulDarcy
+# reservoir domain.
 
-Implementation is provided by `JutulDarcyGeoStatsExt` when `GeoStats` is loaded.
-"""
-function map_to_domain end
+# Implementation is provided by `JutulDarcyGeoStatsExt` when `GeoStats` is loaded.
+# """
+# function map_to_domain end
 
-"""
-    map_to_domain!(domain, property, name; mapping = :mean, info_level = 0, name = missing)
+# """
+#     map_to_domain!(domain, property, name; mapping = :mean, info_level = 0, name = missing)
 
-Mutating variant of `map_to_domain` that stores the mapped values in the named
-property on the domain.
-"""
-function map_to_domain! end
+# Mutating variant of `map_to_domain` that stores the mapped values in the named
+# property on the domain.
+# """
+# function map_to_domain! end
