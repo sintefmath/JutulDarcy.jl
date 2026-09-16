07948a447cb2ae5babed10614c3ce913b610b1f2
Olav Møyner <olav.moyner@gmail.com>
2026-09-16 16:00:26 +0200

    Update linsolve.jl

--- src/linsolve.jl
+++ src/linsolve.jl
@@ -132,7 +132,7 @@ function select_reservoir_linear_solver(model, precond = :cpr;
                 cpr_type = :true_impes
             end
         end
-        is_serial = Threads.nthreads()
+        is_serial = Threads.nthreads() == 1
         if is_serial && is_ka_backend && !is_accelerator_ka
             default_pressure_smoother_type = :gauss_seidel
         else
